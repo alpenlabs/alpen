bbade2245709e757232da65f6cd02d78391c03d3
Sapin Bajracharya <sapin@alpenlabs.io>
2025-06-02 15:27:14 +0545

    fix: add new schema for checkpoints migrate old checkpoints to new schema

--- crates/rocksdb-store/src/checkpoint/db.rs
+++ crates/rocksdb-store/src/checkpoint/db.rs
@@ -4,10 +4,10 @@ use rockbound::{OptimisticTransactionDB, SchemaDBOperationsExt};
 use strata_db::{
     traits::{CheckpointProvider, CheckpointStore},
     types::CheckpointEntry,
-    DbResult,
+    DbError, DbResult,
 };
 
-use super::schemas::BatchCheckpointSchema;
+use super::schemas::{BatchCheckpointIndexedSchema, BatchCheckpointSchema};
 use crate::DbOpsConfig;
 
 pub struct RBCheckpointDB {
@@ -24,6 +24,10 @@ impl RBCheckpointDB {
     pub fn new(db: Arc<OptimisticTransactionDB>, ops: DbOpsConfig) -> Self {
         Self { db, ops }
     }
+
+    fn get_batch_checkpoint_old(&self, batchidx: u64) -> DbResult<Option<CheckpointEntry>> {
+        Ok(self.db.get::<BatchCheckpointSchema>(&batchidx)?)
+    }
 }
 
 impl CheckpointStore for RBCheckpointDB {
@@ -34,17 +38,51 @@ impl CheckpointStore for RBCheckpointDB {
     ) -> DbResult<()> {
         Ok(self
             .db
-            .put::<BatchCheckpointSchema>(&batchidx, &batch_checkpoint)?)
+            .put::<BatchCheckpointIndexedSchema>(&batchidx, &batch_checkpoint)?)
+    }
+
+    fn migrate_checkpoint_data(&self) -> DbResult<(u64, u64)> {
+        // only check for sequential entries
+        let mut batchidx = 0;
+        let mut migrated_count = 0;
+        while let Some(checkpoint) = self.get_batch_checkpoint_old(batchidx)? {
+            if self.get_batch_checkpoint(batchidx)?.is_none() {
+                self.put_batch_checkpoint(batchidx, checkpoint)?;
+                migrated_count += 1;
+            }
+
+            batchidx += 1;
+        }
+
+        // check migration is completed
+        // if old schema has data, ensure new schema is caught up to it
+        if let Some(last_batch_idx_old) = batchidx.checked_sub(1) {
+            match self.get_last_batch_idx()? {
+                Some(last_batch_idx) if last_batch_idx >= last_batch_idx_old => {
+                    // OK
+                }
+                Some(last_batch_idx) => {
+                    return Err(DbError::CheckpointMigrationError(
+                        last_batch_idx_old,
+                        Some(last_batch_idx),
+                    ));
+                }
+                None => {
+                    return Err(DbError::CheckpointMigrationError(last_batch_idx_old, None));
+                }
+            }
+        }
+        Ok((batchidx, migrated_count))
     }
 }
 
 impl CheckpointProvider for RBCheckpointDB {
     fn get_batch_checkpoint(&self, batchidx: u64) -> DbResult<Option<CheckpointEntry>> {
-        Ok(self.db.get::<BatchCheckpointSchema>(&batchidx)?)
+        Ok(self.db.get::<BatchCheckpointIndexedSchema>(&batchidx)?)
     }
 
     fn get_last_batch_idx(&self) -> DbResult<Option<u64>> {
-        Ok(rockbound::utils::get_last::<BatchCheckpointSchema>(&*self.db)?.map(|(x, _)| x))
+        Ok(rockbound::utils::get_last::<BatchCheckpointIndexedSchema>(&*self.db)?.map(|(x, _)| x))
     }
 }
 
