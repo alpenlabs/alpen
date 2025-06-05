7eb3c6d14a81acd46df10b9ac81c22d9f550720a
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-06-05 18:21:06 +0545

    impl mock tx extraction

--- crates/asm/subprotocols/upgrade/src/actions/multisig_update.rs
+++ crates/asm/subprotocols/upgrade/src/actions/multisig_update.rs
@@ -1,17 +1,42 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use strata_asm_common::{MsgRelayer, TxInput};
 
-use crate::{error::UpgradeError, roles::Role, state::UpgradeSubprotoState};
+use crate::{
+    error::UpgradeError,
+    roles::Role,
+    state::UpgradeSubprotoState,
+    types::{PubKey, Signature},
+    vote::AggregatedVote,
+};
+
+pub const MULTISIG_CONFIG_UPDATE_TX_TYPE: u8 = 1;
+
 /// Represents a change to the multisig configuration for the given `role`:
 /// * removes the specified `old_members` from the set,
 /// * adds the specified `new_members`
 /// * updates the threshold.
 #[derive(Debug, Clone, Eq, PartialEq, BorshSerialize, BorshDeserialize)]
-pub struct MultisigConfigUpdate<T: BorshSerialize + BorshDeserialize> {
-    new_members: Vec<T>,
-    old_members: Vec<T>,
+pub struct MultisigConfigUpdate {
+    new_members: Vec<PubKey>,
+    old_members: Vec<PubKey>,
     new_threshold: u8,
     role: Role,
+}
+
+impl MultisigConfigUpdate {
+    pub fn new(
+        new_members: Vec<PubKey>,
+        old_members: Vec<PubKey>,
+        new_threshold: u8,
+        role: Role,
+    ) -> Self {
+        Self {
+            new_members,
+            old_members,
+            new_threshold,
+            role,
+        }
+    }
 }
 
 pub fn handle_multisig_config_update(
@@ -20,4 +45,17 @@ pub fn handle_multisig_config_update(
     relayer: &mut impl MsgRelayer,
 ) -> Result<(), UpgradeError> {
     Ok(())
+}
+
+// FIXME: This is a placeholder for now
+fn extract_multisig_update(
+    tx: &TxInput<'_>,
+) -> Result<(MultisigConfigUpdate, AggregatedVote), UpgradeError> {
+    // sanity check
+    assert_eq!(tx.tag().tx_type(), MULTISIG_CONFIG_UPDATE_TX_TYPE);
+
+    let action = MultisigConfigUpdate::new(vec![], vec![], 0, Role::BridgeAdmin);
+    let vote = AggregatedVote::new(vec![0u8; 15], Signature::default());
+
+    Ok((action, vote))
 }
--- crates/asm/subprotocols/upgrade/src/actions/operator_update.rs
+++ crates/asm/subprotocols/upgrade/src/actions/operator_update.rs
@@ -1,15 +1,31 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use strata_asm_common::{MsgRelayer, TxInput};
 
-use crate::{error::UpgradeError, state::UpgradeSubprotoState};
+use crate::{
+    error::UpgradeError,
+    state::UpgradeSubprotoState,
+    types::{PubKey, Signature},
+    vote::AggregatedVote,
+};
+
+pub const OPERATOR_UPDATE_TX_TYPE: u8 = 3;
 
 /// Represents a change to the Bridge Operator Set`
 /// * removes the specified `old_members` from the set
 /// * adds the specified `new_members`
 #[derive(Debug, Clone, Eq, PartialEq, BorshSerialize, BorshDeserialize)]
-pub struct OperatorSetUpdate<T: BorshSerialize + BorshDeserialize> {
-    new_members: Vec<T>,
-    old_members: Vec<T>,
+pub struct OperatorSetUpdate {
+    new_members: Vec<PubKey>,
+    old_members: Vec<PubKey>,
+}
+
+impl OperatorSetUpdate {
+    pub fn new(new_members: Vec<PubKey>, old_members: Vec<PubKey>) -> Self {
+        Self {
+            new_members,
+            old_members,
+        }
+    }
 }
 
 pub fn handle_operator_update(
@@ -18,4 +34,17 @@ pub fn handle_operator_update(
     relayer: &mut impl MsgRelayer,
 ) -> Result<(), UpgradeError> {
     Ok(())
+}
+
+// FIXME: This is a placeholder for now
+fn extract_multisig_update(
+    tx: &TxInput<'_>,
+) -> Result<(OperatorSetUpdate, AggregatedVote), UpgradeError> {
+    // sanity check
+    assert_eq!(tx.tag().tx_type(), OPERATOR_UPDATE_TX_TYPE);
+
+    let action = OperatorSetUpdate::new(vec![], vec![]);
+    let vote = AggregatedVote::new(vec![0u8; 15], Signature::default());
+
+    Ok((action, vote))
 }
