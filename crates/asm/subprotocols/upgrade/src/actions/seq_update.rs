28e6c90eb72dbf506ac78bd17479a1039360e32c
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-06-05 17:52:56 +0545

    impl basic process txs

--- crates/asm/subprotocols/upgrade/src/actions/seq_update.rs
+++ crates/asm/subprotocols/upgrade/src/actions/seq_update.rs
@@ -1,6 +1,17 @@
 use borsh::{BorshDeserialize, BorshSerialize};
+use strata_asm_common::{MsgRelayer, TxInput};
+
+use crate::{error::UpgradeError, state::UpgradeSubprotoState};
 
 #[derive(Debug, Clone, Eq, PartialEq, BorshSerialize, BorshDeserialize)]
 pub struct SequencerUpdate<T: BorshSerialize + BorshDeserialize> {
     new_sequencer_pub_key: T,
+}
+
+pub fn handle_sequencer_update(
+    state: &mut UpgradeSubprotoState,
+    tx: &TxInput<'_>,
+    relayer: &mut impl MsgRelayer,
+) -> Result<(), UpgradeError> {
+    Ok(())
 }
