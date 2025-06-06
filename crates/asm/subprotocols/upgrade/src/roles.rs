4cd081472a9b35fa2e1a1e16f54304dc515e8dd0
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-06-06 15:13:50 +0545

    feat: represent Role enum by u8

--- crates/asm/subprotocols/upgrade/src/roles.rs
+++ crates/asm/subprotocols/upgrade/src/roles.rs
@@ -1,9 +1,11 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 
 /// Roles with authority in the upgrade subprotocol.
+#[repr(u8)]
 #[derive(
     Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord, Hash, BorshSerialize, BorshDeserialize,
 )]
+#[borsh(use_discriminant = false)]
 pub enum Role {
     /// The multisig authority that has the exclusive ability to update
     /// (add/remove) bridge operators.
@@ -24,4 +26,21 @@ pub enum Role {
 pub enum StrataProof {
     ASM,
     OlStf,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_role_variants_contigous() {
+        // There are 4 variants.
+        let count = 4;
+        // let count = std::mem::variant_count::<Role>() as u8; // This is not available in stable
+        // Rust, so we use a constant.
+
+        for i in 0..count {
+            let role: Role = unsafe { std::mem::transmute(i) };
+            assert_eq!(role as u8, i);
+        }
+    }
 }
