b73a34b4dea49ee6e89b6dcaddd437588e90947b
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-06-08 21:16:39 +0545

    refactor: use Buf32 for PubKey and Buf64 for Signature

--- crates/asm/subprotocols/upgrade/src/crypto.rs
+++ crates/asm/subprotocols/upgrade/src/crypto.rs
@@ -1,63 +1,11 @@
 //! FIXME: All of the code here is only meant as placeholder for now. This needs to based on the
 //! strata-crypto crate
-use borsh::{BorshDeserialize, BorshSerialize};
-use strata_primitives::buf::Buf32;
+use strata_primitives::buf::{Buf32, Buf64};
 
 use crate::error::VoteValidationError;
 
-/// Macro to define a newtype wrapper around `Vec<u8>` with common implementations.
-macro_rules! define_byte_wrapper {
-    ($name:ident) => {
-        /// A type wrapping a [`Vec<u8>`] with common trait implementations,
-        /// allowing easy serialization, comparison, and other utility operations.
-        #[derive(Debug, Clone, BorshSerialize, BorshDeserialize, PartialEq, Eq, Default)]
-        pub struct $name(Vec<u8>);
-
-        impl $name {
-            /// Creates a new instance from a `Vec<u8>`.
-            pub fn new(data: Vec<u8>) -> Self {
-                Self(data)
-            }
-
-            /// Returns a reference to the inner byte slice.
-            pub fn as_bytes(&self) -> &[u8] {
-                &self.0
-            }
-
-            /// Consumes the wrapper and returns the inner `Vec<u8>`.
-            pub fn into_inner(self) -> Vec<u8> {
-                self.0
-            }
-
-            /// Checks if the byte vector is empty.
-            pub fn is_empty(&self) -> bool {
-                self.0.is_empty()
-            }
-        }
-
-        impl From<$name> for Vec<u8> {
-            fn from(value: $name) -> Self {
-                value.0
-            }
-        }
-
-        impl From<&$name> for Vec<u8> {
-            fn from(value: &$name) -> Self {
-                value.0.clone()
-            }
-        }
-
-        impl From<&[u8]> for $name {
-            fn from(value: &[u8]) -> Self {
-                Self(value.to_vec())
-            }
-        }
-    };
-}
-
-// Use the macro to define the specific types.
-define_byte_wrapper!(PubKey);
-define_byte_wrapper!(Signature);
+pub type PubKey = Buf32;
+pub type Signature = Buf64;
 
 // FIXME: handle
 pub fn aggregate_pubkeys(_keys: &[PubKey]) -> Result<PubKey, VoteValidationError> {
