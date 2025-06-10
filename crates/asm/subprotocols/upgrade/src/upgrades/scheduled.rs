e413c6f7fcf3632183f13605324a2d5be3bbb874
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-06-10 10:41:10 +0545

    feat: use activation height instead of blocks_remaining

--- crates/asm/subprotocols/upgrade/src/upgrades/scheduled.rs
+++ crates/asm/subprotocols/upgrade/src/upgrades/scheduled.rs
@@ -32,6 +32,21 @@ impl TryFrom<UpgradeAction> for ScheduledUpgrade {
     }
 }
 
+impl ScheduledUpgrade {
+    pub fn try_new(action: UpgradeAction, current_height: u64) -> Result<Self, UpgradeActionError> {
+        let delay = match &action {
+            UpgradeAction::VerifyingKey(_) => Err(UpgradeActionError::CannotSchedule)?,
+            UpgradeAction::Multisig(_) => Ok(MULTISIG_CONFIG_UPDATE_ENACTMENT_DELAY),
+            UpgradeAction::OperatorSet(_) => Ok(OPERATOR_UPDATE_ENACTMENT_DELAY),
+            UpgradeAction::Sequencer(_) => Ok(SEQUENCER_UPDATE_ENACTMENT_DELAY),
+        }?;
+        let id = action.compute_id();
+        let activation_height = current_height + delay;
+
+        Ok(Self::new(id, action, activation_height))
+    }
+}
+
 impl From<CommittedUpgrade> for ScheduledUpgrade {
     fn from(committed: CommittedUpgrade) -> Self {
         let (id, action) = committed.into_id_and_action();
