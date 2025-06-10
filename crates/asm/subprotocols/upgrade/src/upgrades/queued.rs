c06d2e253140dd7401920a1f3995fb595541a6a9
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-06-10 15:59:28 +0545

    refactor: constants

--- crates/asm/subprotocols/upgrade/src/upgrades/queued.rs
+++ crates/asm/subprotocols/upgrade/src/upgrades/queued.rs
@@ -4,15 +4,16 @@ use strata_asm_proto_upgrade_txs::{
     roles::ProofType,
 };
 
-use crate::{error::UpgradeActionError, upgrades::delayed::DelayedUpgrade};
+use crate::{
+    constants::{ASM_VK_QUEUE_DELAY, OL_STF_VK_QUEUE_DELAY},
+    error::UpgradeActionError,
+    upgrades::delayed::DelayedUpgrade,
+};
 
 #[derive(Debug, Clone, Eq, PartialEq, BorshSerialize, BorshDeserialize)]
 pub struct QueueDelay;
 
 pub type QueuedUpgrade = DelayedUpgrade<QueueDelay>;
-
-pub const ASM_VK_QUEUE_DELAY: u64 = 12_960;
-pub const OL_STF_VK_QUEUE_DELAY: u64 = 4_320;
 
 impl QueuedUpgrade {
     pub fn try_new(
