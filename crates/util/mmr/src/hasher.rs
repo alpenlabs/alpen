a2b52a8c4ce3f38bf015ed75ccb04cd70495e03a
Trey Del Bonis <trey@alpenlabs.io>
2025-06-12 14:04:24 -0400

    fix(mmr): add hash type prefix tagging

--- crates/util/mmr/src/hasher.rs
+++ crates/util/mmr/src/hasher.rs
@@ -1,10 +1,28 @@
 use digest::{generic_array::GenericArray, Digest};
+use lazy_static::lazy_static;
+use sha2::Sha256;
 
 /// 20 byte hash.
 pub type Hash20 = [u8; 20];
 
 /// 32 byte hash.
 pub type Hash32 = [u8; 32];
+
+type Tag = [u8; 64];
+
+lazy_static! {
+    static ref NODE_TAG_PREFIX: Tag = make_tag(b"node");
+    static ref LEAF_TAG_PREFIX: Tag = make_tag(b"leaf");
+}
+
+/// Makes a 64 byte tag from a slice, which ideally contains a ASCII string.
+fn make_tag(s: &[u8]) -> Tag {
+    let raw = Sha256::digest(s);
+    let mut buf = [0; 64];
+    buf[..32].copy_from_slice(&raw);
+    buf[32..].copy_from_slice(&raw);
+    buf
+}
 
 /// Hash wrapper trait.
 pub trait MerkleHash: Copy + Clone + Eq {
@@ -36,9 +54,14 @@ pub trait MerkleHasher {
     /// Hash value.
     type Hash: MerkleHash;
 
-    /// combines the left and Right nodes to form a single Node
+    /// Hashes an arbitrary message as leaf data to compute a leaf hash.
+    fn hash_leaf(buf: &[u8]) -> Self::Hash;
+
+    /// Hashes a node's left and right children to compute the node's hash.
     fn hash_node(left: Self::Hash, right: Self::Hash) -> Self::Hash;
 
+    /// Convenience function that returns a zero hash from the associated hash
+    /// type.
     fn zero_hash() -> Self::Hash {
         <Self::Hash as MerkleHash>::zero()
     }
@@ -49,8 +72,23 @@ pub trait MerkleHasher {
 impl<D: Digest> MerkleHasher for D {
     type Hash = Hash32;
 
+    fn hash_leaf(buf: &[u8]) -> Self::Hash {
+        // This is technically vulnerable to length-extension, but in MMRs that
+        // should not matter, and we use the prefix to prevent type confusion.
+        let mut context = D::new();
+        context.update(*LEAF_TAG_PREFIX);
+        context.update(buf);
+
+        let result = context.finalize();
+        result
+            .as_slice()
+            .try_into()
+            .expect("mmr: digest output not 32 bytes")
+    }
+
     fn hash_node(left: Self::Hash, right: Self::Hash) -> Self::Hash {
         let mut context = D::new();
+        context.update(*NODE_TAG_PREFIX);
         context.update(left);
         context.update(right);
 
