3b54ffb33562440a3adae41ddaa58807678bbb34
Bibek Pandey <bibek@alpenlabs.io>
2025-06-18 14:59:33 +0545

    (func-tests): Add timeout prover_checkpoint_latest test

--- functional-tests/tests/prover/prover_checkpoint_latest.py
+++ functional-tests/tests/prover/prover_checkpoint_latest.py
@@ -26,7 +26,7 @@ class ProverClientTest(testenv.StrataTestBase):
         self.debug(f"using task id: {task_id}")
         assert task_id is not None
 
-        time_out = 30
+        time_out = 40
         is_proof_generation_completed = wait_for_proof_with_time_out(
             prover_client_rpc, task_id, time_out=time_out
         )
