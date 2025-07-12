9a973a99a891e5c590b2fe75f42081ba2b443443
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-07-12 13:31:12 +0545

    refactor: move handling of aux bundle to process stage

--- crates/asm/stf/src/stage.rs
+++ crates/asm/stf/src/stage.rs
@@ -10,19 +10,13 @@ use crate::manager::SubprotoManager;
 /// Stage that loads each subprotocol from the anchor state we're basing off of.
 pub(crate) struct SubprotoLoaderStage<'a> {
     anchor_state: &'a AnchorState,
-    aux_bundle: &'a AuxBundle,
     manager: &'a mut SubprotoManager,
 }
 
 impl<'a> SubprotoLoaderStage<'a> {
-    pub(crate) fn new(
-        anchor_state: &'a AnchorState,
-        aux_bundle: &'a AuxBundle,
-        manager: &'a mut SubprotoManager,
-    ) -> Self {
+    pub(crate) fn new(anchor_state: &'a AnchorState, manager: &'a mut SubprotoManager) -> Self {
         Self {
             anchor_state,
-            aux_bundle,
             manager,
         }
     }
@@ -39,40 +33,35 @@ impl Stage for SubprotoLoaderStage<'_> {
             None => S::init(),
         };
 
-        // OPTIMIZE: Linear scan is done every time to find the section
-        let aux_inputs = match self.aux_bundle.find_payload(S::ID) {
-            Some(sec) => sec
-                .try_to_aux_inputs::<S>()
-                .expect("asm: invalid aux inputs"),
-            None => Vec::new(),
-        };
-
-        self.manager.insert_subproto::<S>(state, aux_inputs);
+        self.manager.insert_subproto::<S>(state);
     }
 }
 
 /// Stage to process txs pre-extracted from the block for each subprotocol.
-pub(crate) struct ProcessStage<'a, 'b, 'm> {
+pub(crate) struct ProcessStage<'a, 'b, 'm, 'x> {
     anchor_state: &'a AnchorState,
     tx_bufs: BTreeMap<SubprotocolId, Vec<TxInput<'b>>>,
+    aux: &'x AuxBundle,
     manager: &'m mut SubprotoManager,
 }
 
-impl<'a, 'b, 'm> ProcessStage<'a, 'b, 'm> {
+impl<'a, 'b, 'm, 'x> ProcessStage<'a, 'b, 'm, 'x> {
     pub(crate) fn new(
         tx_bufs: BTreeMap<SubprotocolId, Vec<TxInput<'b>>>,
+        aux: &'x AuxBundle,
         manager: &'m mut SubprotoManager,
         anchor_state: &'a AnchorState,
     ) -> Self {
         Self {
             anchor_state,
             tx_bufs,
+            aux,
             manager,
         }
     }
 }
 
-impl Stage for ProcessStage<'_, '_, '_> {
+impl Stage for ProcessStage<'_, '_, '_, '_> {
     fn process_subprotocol<S: Subprotocol>(&mut self) {
         let txs = self
             .tx_bufs
