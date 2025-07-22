6412fa3f3fb767826cd177d1133405a0049b2a6a
Jose Storopoli <jose@storopoli.com>
2025-07-22 08:51:57 -0300

    backport: Automated update to rustc (to nightly-2025-06-01) (#849) (#945)
    Automated update to rustc (to nightly-2025-06-01) (#849)
    
    * Automated update to Rust nightly-2025-06-01
    
    * chore: fix clippy lints
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Update Nightly Rustc Bot <no-reply@alpenlabs.io>

--- bin/alpen-cli/src/cmd/faucet.rs
+++ bin/alpen-cli/src/cmd/faucet.rs
@@ -60,7 +60,7 @@ impl fmt::Display for Chain {
             Chain::L1 => "l1",
             Chain::L2 => "l2",
         };
-        write!(f, "{}", chain_str)
+        write!(f, "{chain_str}")
     }
 }
 
@@ -121,8 +121,7 @@ pub async fn faucet(
         .user_error("Invalid faucet endopoint. Check the config file")?;
     base_url = ensure_trailing_slash(base_url);
     let chain = Chain::from_network_type(network_type.clone()).user_error(format!(
-        "Unsupported network {}. Must be `signet` or `alpen`",
-        network_type
+        "Unsupported network {network_type}. Must be `signet` or `alpen`"
     ))?;
     let endpoint = base_url
         .join(&format!("pow_challenge/{chain}"))
@@ -177,7 +176,7 @@ pub async fn faucet(
         "✔ Solved challenge after {solution} attempts. Claiming now."
     ));
 
-    println!("Claiming to {} address {}", network_type, address);
+    println!("Claiming to {network_type} address {address}");
 
     let url = format!(
         "{base_url}{}/{}/{}",
