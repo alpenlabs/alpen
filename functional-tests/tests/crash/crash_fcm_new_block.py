2330ad98ce9b062e2ad9e6593a490d9418d6798c
Sapin Bajracharya <sapin@alpenlabs.io>
2025-07-22 16:07:12 +0545

    fn-test: allow different recovery metric for crash mixin

--- functional-tests/tests/crash/crash_fcm_new_block.py
+++ functional-tests/tests/crash/crash_fcm_new_block.py
@@ -6,7 +6,7 @@ from utils import wait_until
 
 
 @flexitest.register
-class CrashFcmNewBlockTest(seq_crash_mixin.SeqCrashMixin):
+class CrashFcmNewBlockTest(seq_crash_mixin.DefaultSeqCrashMixin):
     def __init__(self, ctx: flexitest.InitContext):
         ctx.set_env(testenv.BasicEnvConfig(101))
 
@@ -14,7 +14,7 @@ class CrashFcmNewBlockTest(seq_crash_mixin.SeqCrashMixin):
         cur_chain_tip = self.handle_bail(lambda: "fcm_new_block")
 
         wait_until(
-            lambda: self.seqrpc.strata_clientStatus()["chain_tip_slot"] > cur_chain_tip + 1,
+            lambda: self.get_recovery_metric() > cur_chain_tip + 1,
             error_with="chain tip slot not progressing",
             timeout=20,
         )
