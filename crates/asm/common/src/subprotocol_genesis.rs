4200ed007115bd1703fc6158468044d3ac317358
Hamid Bateni <hamidmrd79@gmail.com>
2025-07-23 15:33:14 +0400

    refactor(asm): fix lint issues

--- crates/asm/common/src/subprotocol_genesis.rs
+++ crates/asm/common/src/subprotocol_genesis.rs
@@ -22,10 +22,8 @@ pub struct GenesisConfigRegistry {
     configs: BTreeMap<SubprotocolId, Vec<u8>>,
 }
 
-/// [PLACE_HOLDER] =>
-/// TODO: Do we have a better alternative approach that simplifies and improves state
-/// initialization?
-/// the current approach add genesis management complexity to ASM upper layer
+// [PLACE_HOLDER] => TODO: Do we have a better alternative approach that simplifies and improves
+// state initialization? the current approach add genesis management complexity to ASM upper layer
 impl GenesisConfigRegistry {
     pub fn new() -> Self {
         Self {
