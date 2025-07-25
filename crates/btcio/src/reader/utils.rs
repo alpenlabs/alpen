52538bc0416c483e4578e9f29da86e1e8e6bb2d6
Evgeniy Zdanovich <zdanovichevgeny@gmail.com>
2025-07-25 17:01:37 +0200

    chore: cleanups around btcio::L1Event and removal of dead code.

--- crates/btcio/src/reader/utils.rs
+++ crates/btcio/src/reader/utils.rs
@@ -24,14 +24,13 @@ pub(crate) fn find_checkpoint(entries: &[RelevantTxEntry]) -> Option<&SignedChec
 }
 
 /// Looks for checkpoints in given L1 events
-pub(crate) fn find_checkpoint_in_events(evs: &[L1Event]) -> Option<&SignedCheckpoint> {
-    for ev in evs {
-        if let L1Event::BlockData(blk_data, _epoch) = ev {
-            if let Some(checkpt) = find_checkpoint(blk_data.relevant_txs()) {
-                return Some(checkpt);
-            }
+pub(crate) fn find_checkpoint_in_event(ev: &L1Event) -> Option<&SignedCheckpoint> {
+    if let L1Event::BlockData(blk_data, _epoch) = ev {
+        if let Some(checkpt) = find_checkpoint(blk_data.relevant_txs()) {
+            return Some(checkpt);
         }
     }
+
     None
 }
 
