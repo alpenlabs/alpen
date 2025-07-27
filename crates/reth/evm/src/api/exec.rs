7c682de5f2d12d24029506bf2ea43d4e65e7a1c2
abishek <abishek@alpenlabs.io>
2025-07-27 22:56:45 +0545

    refactor: remove alpen evm inner

--- crates/reth/evm/src/api/exec.rs
+++ crates/reth/evm/src/api/exec.rs
@@ -1,90 +1,78 @@
+use reth_evm::{eth::EthEvmContext, Database};
 use revm::{
-    context::{
-        result::{EVMError, ExecResultAndState, ExecutionResult, HaltReason, InvalidTransaction},
-        ContextSetters, ContextTr, JournalTr,
+    context::{BlockEnv, ContextSetters, TxEnv},
+    context_interface::{
+        result::{EVMError, ExecutionResult, ResultAndState},
+        ContextTr,
     },
-    handler::{instructions::EthInstructions, EvmTr, Handler},
-    inspector::{InspectorHandler, JournalExt},
-    interpreter::interpreter::EthInterpreter,
+    handler::Handler,
+    inspector::{InspectCommitEvm, InspectEvm, Inspector, InspectorHandler},
     state::EvmState,
-    Database, DatabaseCommit, ExecuteCommitEvm, ExecuteEvm, InspectCommitEvm, InspectEvm,
-    Inspector,
+    DatabaseCommit, ExecuteCommitEvm, ExecuteEvm,
 };
 
-use crate::api::{evm::AlpenEvmInner, handler::AlpenRevmHandler};
+use crate::api::{handler::AlpenRevmHandler, AlpenEvm};
 
-/// Type alias for the error type of the AlpenEvm.
-type AlpenEvmError<CTX> = EVMError<<<CTX as ContextTr>::Db as Database>::Error, InvalidTransaction>;
-
-// Trait that allows to replay and transact the transaction.
-impl<CTX, INSP> ExecuteEvm for AlpenEvmInner<CTX, INSP, EthInstructions<EthInterpreter, CTX>>
+impl<DB, INSP> ExecuteEvm for AlpenEvm<DB, INSP>
 where
-    CTX: ContextSetters<Journal: JournalTr<State = EvmState>>,
+    DB: Database,
 {
+    type ExecutionResult = ExecutionResult;
     type State = EvmState;
-    type ExecutionResult = ExecutionResult<HaltReason>;
-    type Error = AlpenEvmError<CTX>;
-
-    type Tx = <CTX as ContextTr>::Tx;
-
-    type Block = <CTX as ContextTr>::Block;
+    type Error = EVMError<DB::Error>;
+    type Tx = TxEnv;
+    type Block = BlockEnv;
 
     fn set_block(&mut self, block: Self::Block) {
-        self.evm_ctx.ctx.set_block(block);
-    }
-    fn replay(
-        &mut self,
-    ) -> Result<ExecResultAndState<Self::ExecutionResult, Self::State>, Self::Error> {
-        let mut handler = AlpenRevmHandler::default();
-        handler.run(self).map(|result| {
-            let state = self.finalize();
-            ExecResultAndState::new(result, state)
-        })
+        self.inner.set_block(block);
     }
 
     fn transact_one(&mut self, tx: Self::Tx) -> Result<Self::ExecutionResult, Self::Error> {
-        self.evm_ctx.ctx.set_tx(tx);
-        let mut handler = AlpenRevmHandler::default();
-        handler.run(self)
+        self.inner.ctx.set_tx(tx);
+        AlpenRevmHandler::default().run(self)
     }
 
     fn finalize(&mut self) -> Self::State {
-        self.ctx().journal_mut().finalize()
+        self.inner.finalize()
+    }
+
+    fn replay(&mut self) -> Result<ResultAndState, Self::Error> {
+        AlpenRevmHandler::default().run(self).map(|result| {
+            let state = self.finalize();
+            ResultAndState::new(result, state)
+        })
     }
 }
 
-// Trait allows replay_commit and transact_commit functionality.
-impl<CTX, INSP> ExecuteCommitEvm for AlpenEvmInner<CTX, INSP>
+impl<DB, INSP> ExecuteCommitEvm for AlpenEvm<DB, INSP>
 where
-    CTX: ContextSetters<Db: DatabaseCommit, Journal: JournalTr<State = EvmState>>,
+    DB: Database + DatabaseCommit,
 {
     fn commit(&mut self, state: Self::State) {
-        self.ctx().db_mut().commit(state);
+        self.inner.ctx.db_mut().commit(state);
     }
 }
 
-impl<CTX, INSP> InspectEvm for AlpenEvmInner<CTX, INSP>
+impl<DB, INSP> InspectEvm for AlpenEvm<DB, INSP>
 where
-    CTX: ContextSetters<Journal: JournalTr<State = EvmState> + JournalExt>,
-    INSP: Inspector<CTX, EthInterpreter>,
+    DB: Database,
+    INSP: Inspector<EthEvmContext<DB>>,
 {
     type Inspector = INSP;
 
     fn set_inspector(&mut self, inspector: Self::Inspector) {
-        self.evm_ctx.inspector = inspector;
+        self.inner.set_inspector(inspector);
     }
 
     fn inspect_one_tx(&mut self, tx: Self::Tx) -> Result<Self::ExecutionResult, Self::Error> {
-        self.evm_ctx.ctx.set_tx(tx);
-        let mut handler = AlpenRevmHandler::default();
-        handler.inspect_run(self)
+        self.inner.ctx.set_tx(tx);
+        AlpenRevmHandler::default().inspect_run(self)
     }
 }
 
-// Inspect
-impl<CTX, INSP> InspectCommitEvm for AlpenEvmInner<CTX, INSP>
+impl<DB, INSP> InspectCommitEvm for AlpenEvm<DB, INSP>
 where
-    CTX: ContextSetters<Db: DatabaseCommit, Journal: JournalTr<State = EvmState> + JournalExt>,
-    INSP: Inspector<CTX, EthInterpreter>,
+    DB: Database + DatabaseCommit,
+    INSP: Inspector<EthEvmContext<DB>>,
 {
 }
--- crates/reth/evm/src/evm.rs
+++ crates/reth/evm/src/evm.rs
@@ -1,32 +1,18 @@
-use std::{
-    ops::{Deref, DerefMut},
-    sync::OnceLock,
-};
+use std::sync::OnceLock;
 
-use reth_evm::{
-    eth::EthEvmContext, precompiles::PrecompilesMap, Database, Evm, EvmEnv, EvmFactory,
-};
+use reth_evm::{eth::EthEvmContext, precompiles::PrecompilesMap, EvmEnv, EvmFactory};
 use revm::{
-    context::{BlockEnv, Cfg, ContextTr, TxEnv},
-    context_interface::result::{EVMError, HaltReason, ResultAndState},
-    handler::{instructions::EthInstructions, EthPrecompiles, PrecompileProvider},
+    context::{result::HaltReason, Cfg, ContextTr, TxEnv},
+    context_interface::result::EVMError,
+    handler::{EthPrecompiles, PrecompileProvider},
     inspector::NoOpInspector,
-    interpreter::{
-        interpreter::EthInterpreter, Gas, InputsImpl, InstructionResult, InterpreterResult,
-    },
-    precompile::{bls12_381, PrecompileError, PrecompileFn, Precompiles},
-    Context, Inspector, MainBuilder, MainContext,
+    interpreter::{Gas, InputsImpl, InstructionResult, InterpreterResult},
+    precompile::{bls12_381, Precompiles},
+    Context, MainBuilder, MainContext,
 };
-use revm_primitives::{hardfork::SpecId, Address, Bytes, TxKind, U256};
+use revm_primitives::{hardfork::SpecId, Address, Bytes};
 
-use crate::{
-    api::evm::AlpenEvmInner,
-    constants::{BRIDGEOUT_ADDRESS, SCHNORR_ADDRESS},
-    precompiles::{
-        bridge::{bridge_context_call, bridgeout_precompile},
-        schnorr::verify_schnorr_precompile,
-    },
-};
+use crate::api::AlpenEvm;
 
 /// A custom precompile that contains static precompiles.
 #[allow(missing_debug_implementations)]
@@ -128,8 +114,7 @@ impl<CTX: ContextTr> PrecompileProvider<CTX> for AlpenEvmPrecompiles {
 pub struct AlpenEvmFactory;
 
 impl EvmFactory for AlpenEvmFactory {
-    type Evm<DB: reth_evm::Database, I: revm::Inspector<Self::Context<DB>>> =
-        AlpenEvm<DB, I, Self::Precompiles>;
+    type Evm<DB: reth_evm::Database, I: revm::Inspector<Self::Context<DB>>> = AlpenEvm<DB, I>;
 
     type Context<DB: reth_evm::Database> = EthEvmContext<DB>;
 
@@ -158,7 +143,7 @@ impl EvmFactory for AlpenEvmFactory {
             ));
 
         AlpenEvm {
-            inner: AlpenEvmInner::new(evm_ctx),
+            inner: evm_ctx,
             inspect: false,
         }
     }
@@ -180,174 +165,8 @@ impl EvmFactory for AlpenEvmFactory {
             ));
 
         AlpenEvm {
-            inner: AlpenEvmInner::new(evm_ctx),
+            inner: evm_ctx,
             inspect: true,
         }
-    }
-}
-
-/// Alpen EVM implementation.
-#[allow(missing_debug_implementations)]
-pub struct AlpenEvm<DB: Database, I, P = AlpenEvmPrecompiles> {
-    pub inner:
-        AlpenEvmInner<EthEvmContext<DB>, I, EthInstructions<EthInterpreter, EthEvmContext<DB>>, P>,
-    pub inspect: bool,
-}
-impl<DB: Database, I, P> AlpenEvm<DB, I, P> {
-    /// Provides a reference to the EVM context.
-    pub const fn ctx(&self) -> &EthEvmContext<DB> {
-        &self.inner.evm_ctx.ctx
-    }
-
-    /// Provides a mutable reference to the EVM context.
-    pub fn ctx_mut(&mut self) -> &mut EthEvmContext<DB> {
-        &mut self.inner.evm_ctx.ctx
-    }
-}
-
-impl<DB: Database, I, P> Deref for AlpenEvm<DB, I, P> {
-    type Target = EthEvmContext<DB>;
-
-    #[inline]
-    fn deref(&self) -> &Self::Target {
-        self.ctx()
-    }
-}
-
-impl<DB: Database, I, P> DerefMut for AlpenEvm<DB, I, P> {
-    #[inline]
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.ctx_mut()
-    }
-}
-
-/// Implements the AlloyEvm EVM trait for AlpenEvm.
-///
-/// This implementation closely follows the Alloy EVM implementation for Ethereum mainnet,
-/// adapting it for AlpenEvm with custom precompiles and configuration.
-/// <https://github.com/alloy-rs/evm/blob/65bdb46726a4cdb265f13e4ea663a57ecf0f8d6c/crates/evm/src/eth/mod.rs#L104>
-impl<DB, I, P> Evm for AlpenEvm<DB, I, P>
-where
-    DB: Database,
-    I: Inspector<EthEvmContext<DB>>,
-    AlpenEvmPrecompiles: PrecompileProvider<EthEvmContext<DB>, Output = InterpreterResult>,
-{
-    type DB = DB;
-    type Tx = TxEnv;
-    type Error = EVMError<DB::Error>;
-    type HaltReason = HaltReason;
-    type Spec = SpecId;
-    type Precompiles = PrecompilesMap;
-    type Inspector = I;
-
-    fn block(&self) -> &BlockEnv {
-        &self.block
-    }
-
-    fn chain_id(&self) -> u64 {
-        self.cfg.chain_id
-    }
-
-    fn transact_raw(
-        &mut self,
-        _tx: Self::Tx,
-    ) -> Result<ResultAndState<Self::HaltReason>, Self::Error> {
-        // if self.inspect {
-        //     self.ctx().inspect_tx(tx)
-        // } else {
-        //     self.inner.evm_ctx.transact(tx)
-        // }
-        todo!()
-    }
-
-    fn transact_system_call(
-        &mut self,
-        caller: Address,
-        contract: Address,
-        data: Bytes,
-    ) -> Result<ResultAndState<Self::HaltReason>, Self::Error> {
-        let tx = TxEnv {
-            caller,
-            kind: TxKind::Call(contract),
-            // Explicitly set nonce to 0 so revm does not do any nonce checks
-            nonce: 0,
-            gas_limit: 30_000_000,
-            value: U256::ZERO,
-            data,
-            // Setting the gas price to zero enforces that no value is transferred as part of the
-            // call, and that the call will not count against the block's gas limit
-            gas_price: 0,
-            // The chain ID check is not relevant here and is disabled if set to None
-            chain_id: None,
-            // Setting the gas priority fee to None ensures the effective gas price is derived from
-            // the `gas_price` field, which we need to be zero
-            gas_priority_fee: None,
-            access_list: Default::default(),
-            // blob fields can be None for this tx
-            blob_hashes: Vec::new(),
-            max_fee_per_blob_gas: 0,
-            tx_type: 0,
-            authorization_list: Default::default(),
-        };
-
-        let mut gas_limit = tx.gas_limit;
-        let mut basefee = 0;
-        let mut disable_nonce_check = true;
-
-        // ensure the block gas limit is >= the tx
-        core::mem::swap(&mut self.block.gas_limit, &mut gas_limit);
-        // disable the base fee check for this call by setting the base fee to zero
-        core::mem::swap(&mut self.block.basefee, &mut basefee);
-        // disable the nonce check
-        core::mem::swap(&mut self.cfg.disable_nonce_check, &mut disable_nonce_check);
-
-        let mut res = self.transact(tx);
-
-        // swap back to the previous gas limit
-        core::mem::swap(&mut self.block.gas_limit, &mut gas_limit);
-        // swap back to the previous base fee
-        core::mem::swap(&mut self.block.basefee, &mut basefee);
-        // swap back to the previous nonce check flag
-        core::mem::swap(&mut self.cfg.disable_nonce_check, &mut disable_nonce_check);
-
-        // NOTE: We assume that only the contract storage is modified. Revm currently marks the
-        // caller and block beneficiary accounts as "touched" when we do the above transact calls,
-        // and includes them in the result.
-        //
-        // We're doing this state cleanup to make sure that changeset only includes the changed
-        // contract storage.
-        if let Ok(res) = &mut res {
-            res.state.retain(|addr, _| *addr == contract);
-        }
-
-        res
-    }
-
-    fn db_mut(&mut self) -> &mut Self::DB {
-        &mut self.journaled_state.database
-    }
-
-    fn finish(self) -> (Self::DB, EvmEnv<Self::Spec>) {
-        todo!()
-    }
-
-    fn set_inspector_enabled(&mut self, enabled: bool) {
-        self.inspect = enabled;
-    }
-
-    fn precompiles(&self) -> &Self::Precompiles {
-        todo!()
-    }
-
-    fn precompiles_mut(&mut self) -> &mut Self::Precompiles {
-        todo!()
-    }
-
-    fn inspector(&self) -> &Self::Inspector {
-        todo!()
-    }
-
-    fn inspector_mut(&mut self) -> &mut Self::Inspector {
-        todo!()
     }
 }
