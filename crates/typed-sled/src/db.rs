e08d35561c751e7d3da45fd6581b4cfa378689d4
Bibek Pandey <bibek@alpenlabs.io>
2025-07-28 17:46:15 +0545

    (typed-sled): Fix transaction method

--- crates/typed-sled/src/db.rs
+++ crates/typed-sled/src/db.rs
@@ -1,7 +1,10 @@
 use std::sync::Arc;
 
 use dashmap::DashMap;
-use sled::{Db, Transactional, Tree, transaction::ConflictableTransactionError};
+use sled::{
+    Db, Transactional, Tree,
+    transaction::{ConflictableTransactionError, TransactionError},
+};
 
 use crate::{
     error::{Error, Result},
@@ -38,18 +41,27 @@ impl SledDb {
         Ok(SledTree::new(final_tree.clone()))
     }
 
-    pub fn transaction<F, S1: Schema, S2: Schema>(&self, func: F) -> Result<()>
+    pub fn transaction<F, S1: Schema, S2: Schema>(
+        &self,
+        func: F,
+    ) -> core::result::Result<(), sled::transaction::TransactionError<crate::error::Error>>
     where
         F: Fn((&SledTransactionalTree<S1>, &SledTransactionalTree<S2>)) -> Result<()>,
     {
-        let t1: SledTree<S1> = self.get_tree()?;
-        let t2: SledTree<S2> = self.get_tree()?;
-        (&*t1.inner, &*t2.inner)
-            .transaction(|(t1, t2)| {
-                let st1 = SledTransactionalTree::<S1>::new(t1.clone());
-                let st2 = SledTransactionalTree::<S2>::new(t2.clone());
-                func((&st1, &st2)).map_err(ConflictableTransactionError::Abort)
-            })
-            .map_err(|e| Error::TransactionError(e))
+        let t1: SledTree<S1> = self.get_tree().map_err(|e| match e {
+            Error::SledError(sled_err) => TransactionError::Storage(sled_err),
+            other => TransactionError::Storage(sled::Error::Io(std::io::Error::other(other))),
+        })?;
+        let t2: SledTree<S2> = self.get_tree().map_err(|e| match e {
+            Error::SledError(sled_err) => TransactionError::Storage(sled_err),
+            other => sled::transaction::TransactionError::Storage(sled::Error::Io(
+                std::io::Error::other(other),
+            )),
+        })?;
+        (&*t1.inner, &*t2.inner).transaction(|(t1, t2)| {
+            let st1 = SledTransactionalTree::<S1>::new(t1.clone());
+            let st2 = SledTransactionalTree::<S2>::new(t2.clone());
+            func((&st1, &st2)).map_err(ConflictableTransactionError::Abort)
+        })
     }
 }
