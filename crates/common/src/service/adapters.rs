2b083b196ff27f9cb7c8ede78a9892929e301c91
Trey Del Bonis <trey@alpenlabs.io>
2025-08-04 15:38:22 -0400

    feature(common): add more adapter impls

--- crates/common/src/service/adapters.rs
+++ crates/common/src/service/adapters.rs
@@ -1,5 +1,9 @@
 use std::{fmt::Debug, sync::Arc};
 
+use futures::{
+    pin_mut,
+    stream::{Stream, StreamExt},
+};
 use tokio::sync::{mpsc, Mutex};
 
 use super::*;
@@ -120,7 +124,9 @@ where
 }
 
 /// Adapter for using a mpsc receiver as a input.
-// TODO convert to supporting any `Stream`?
+///
+/// This is needed because [``mpsc::Receiver``] does not natively implement
+/// [``Stream``] and it avoids having to use the Tokio wrapper.
 pub struct TokioMpscInput<T> {
     rx: mpsc::Receiver<T>,
     closed: bool,
@@ -146,5 +152,115 @@ impl<T: Debug + Send + 'static> AsyncServiceInput for TokioMpscInput<T> {
         let item = self.rx.recv().await;
         self.closed |= item.is_none();
         Ok(item)
+    }
+}
+
+/// This impl is technically redundant since we can use the type as an
+/// [``Iterator``], but someone might find it useful and it's easy enough to
+/// implement.
+impl<T: Debug + Send + 'static> SyncServiceInput for TokioMpscInput<T> {
+    fn recv_next(&mut self) -> anyhow::Result<Option<Self::Msg>> {
+        // We fuse it off ourselves just in case, it'd be weird not to.
+        if self.closed {
+            return Ok(None);
+        }
+
+        let item = self.rx.blocking_recv();
+        self.closed |= item.is_none();
+        Ok(item)
+    }
+}
+
+/// Adapter for using an arbitrary [``Stream``] impl as an input.
+pub struct StreamInput<S> {
+    stream: S,
+    closed: bool,
+}
+
+impl<S> StreamInput<S> {
+    pub fn new(stream: S) -> Self {
+        Self {
+            stream,
+            closed: false,
+        }
+    }
+}
+
+impl<S: Stream> ServiceInput for StreamInput<S>
+where
+    S::Item: Debug,
+{
+    type Msg = S::Item;
+}
+
+impl<S: Stream + Unpin + Sync + Send + 'static> AsyncServiceInput for StreamInput<S>
+where
+    S::Item: Debug,
+{
+    async fn recv_next(&mut self) -> anyhow::Result<Option<Self::Msg>> {
+        // We fuse it off ourselves just in case, it'd be weird not to.
+        if self.closed {
+            return Ok(None);
+        }
+
+        let item = self.stream.next().await;
+        self.closed |= item.is_none();
+        Ok(item)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::stream;
+    use tokio::sync::mpsc;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_stream_input() {
+        let v = 3;
+        let stream = stream::repeat(v);
+        let mut inp = StreamInput::new(stream);
+
+        let rv = inp
+            .recv_next()
+            .await
+            .expect("test: recv input")
+            .expect("test: have input");
+
+        assert_eq!(rv, v, "test: input match");
+    }
+
+    #[tokio::test]
+    async fn test_mpsc_input_async() {
+        let v = 3;
+
+        let (tx, rx) = mpsc::channel(10);
+        let mut inp = TokioMpscInput::new(rx);
+
+        tx.send(v).await.expect("test: send input");
+
+        let rv = AsyncServiceInput::recv_next(&mut inp)
+            .await
+            .expect("test: recv input")
+            .expect("test: have input");
+
+        assert_eq!(rv, v, "test: input match");
+    }
+
+    #[test]
+    fn test_mpsc_input_blocking() {
+        let v = 3;
+
+        let (tx, rx) = mpsc::channel(10);
+        let mut inp = TokioMpscInput::new(rx);
+
+        tx.blocking_send(v).expect("test: send input");
+
+        let rv = SyncServiceInput::recv_next(&mut inp)
+            .expect("test: recv input")
+            .expect("test: have input");
+
+        assert_eq!(rv, v, "test: input match");
     }
 }
