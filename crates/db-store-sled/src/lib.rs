fb93340a843e5364a740c95aeac0c4d3d5dc089e
Bibek Pandey <bibek@alpenlabs.io>
2025-08-06 12:02:36 +0545

    (db-store-sled): Add client_state

--- crates/db-store-sled/src/lib.rs
+++ crates/db-store-sled/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod broadcaster;
 pub mod chain_state;
 pub mod checkpoint;
+pub mod client_state;
 pub mod l2;
 pub mod macros;
 pub mod utils;
@@ -9,3 +10,4 @@ pub mod utils;
 pub use broadcaster::db::{BroadcastDb, L1BroadcastDBSled};
 pub use chain_state::db::ChainstateDBSled;
 pub use checkpoint::db::CheckpointDBSled;
+pub use client_state::db::ClientStateDBSled;
