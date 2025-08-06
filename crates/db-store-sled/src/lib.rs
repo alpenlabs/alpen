fabf77930447eafbd820cb1f53fcae09df65dd39
Bibek Pandey <bibek@alpenlabs.io>
2025-08-06 13:10:36 +0545

    (db-store-sled): Add Prover db impl

--- crates/db-store-sled/src/lib.rs
+++ crates/db-store-sled/src/lib.rs
@@ -5,6 +5,7 @@ pub mod client_state;
 pub mod l1;
 pub mod l2;
 pub mod macros;
+pub mod prover;
 pub mod utils;
 
 // Re-exports
@@ -13,3 +14,4 @@ pub use chain_state::db::ChainstateDBSled;
 pub use checkpoint::db::CheckpointDBSled;
 pub use client_state::db::ClientStateDBSled;
 pub use l1::db::L1DBSled;
+pub use prover::db::ProofDBSled;
