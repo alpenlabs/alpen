04349b6933d91ba56651618913fa034d2b56f4a9
Bibek Pandey <bibek@alpenlabs.io>
2025-08-15 15:53:30 +0545

    Get rid of rocksdb related crates

--- crates/db-store-rocksdb/src/client_state/schemas.rs
+++
@@ -1,9 +0,0 @@
-use strata_state::operation::ClientUpdateOutput;
-
-use crate::{define_table_with_seek_key_codec, define_table_without_codec, impl_borsh_value_codec};
-
-// Consensus Output Schema and corresponding codecs implementation
-define_table_with_seek_key_codec!(
-    /// Table to store client state updates.
-    (ClientUpdateOutputSchema) u64 => ClientUpdateOutput
-);
