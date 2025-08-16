86b65934f808cdaf3eb773c8f6adce50103b2b6f
Hamid Bateni <hamidmrd79@gmail.com>
2025-08-16 14:39:26 +0400

    fix(functional test): fix prover_client_restart logic

--- functional-tests/tests/prover/prover_client_restart.py
+++ functional-tests/tests/prover/prover_client_restart.py
@@ -37,24 +37,40 @@ class ProverClientRestartTest(StrataTestBase):
             error_with="Prover did not start on time",
         )
 
-        # Prover starts, proof is generated and checkpoint advances
+        # Wait for the first checkpoint to be ready and prove it
         latest_checkpoint = sequencer_rpc.strata_getLatestCheckpointIndex()
+        wait_until(
+            lambda: sequencer_rpc.strata_getLatestCheckpointIndex() == latest_checkpoint + 1,
+            timeout=180,
+            step=5.0,
+        )
+        latest_checkpoint = sequencer_rpc.strata_getLatestCheckpointIndex()
+
+        self.prove_latest_checkpoint(prover_client_rpc)
+
+        self.debug("restart prover client")
+        prover_client.stop()
+        time.sleep(0.5)
+        prover_client.start()
+        prover_client_rpc = prover_client.create_rpc()
+
         self.debug("prover client restarted, waiting for the new checkpoint")
         wait_until(
             lambda: sequencer_rpc.strata_getLatestCheckpointIndex() == latest_checkpoint + 1,
             timeout=180,
             step=5.0,
         )
-
-        # Restart the prover client
-        prover_client.stop()
-        time.sleep(0.5)
         latest_checkpoint = sequencer_rpc.strata_getLatestCheckpointIndex()
-        prover_client.start()
 
-        # After restarting prover, proof is generated and checkpoint advances
-        wait_until(
-            lambda: sequencer_rpc.strata_getLatestCheckpointIndex() == latest_checkpoint + 1,
-            timeout=180,
-            step=5.0,
-        )
+        self.prove_latest_checkpoint(prover_client_rpc)
+
+    def prove_latest_checkpoint(self, prover_client_rpc):
+        task_ids = prover_client_rpc.dev_strata_proveLatestCheckPoint()
+        prover_waiter = self.create_prover_waiter(prover_client_rpc, timeout=30)
+        self.debug(f"got task ids: {task_ids}")
+        task_id = task_ids[0]
+        self.debug(f"using task id: {task_id}")
+        assert task_id is not None
+
+        is_proof_generation_completed = prover_waiter.wait_for_proof_completion(task_id)
+        assert is_proof_generation_completed
