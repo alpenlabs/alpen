018efc6e3c2deea576482603a67e412fef6f0b6a
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-09-04 23:07:09 +0545

    fix: handle pending updates

--- crates/asm/subprotocols/administration/src/handler.rs
+++ crates/asm/subprotocols/administration/src/handler.rs
@@ -1,10 +1,42 @@
 use strata_asm_common::MsgRelayer;
 use strata_asm_proto_administration_txs::actions::{MultisigAction, UpdateAction};
 use strata_crypto::multisig::vote::AggregatedVote;
+use strata_primitives::roles::ProofType;
 
 use crate::{
     error::AdministrationError, queued_update::QueuedUpdate, state::AdministrationSubprotoState,
 };
+
+pub(crate) fn handle_pending_updates(
+    state: &mut AdministrationSubprotoState,
+    _relayer: &mut impl MsgRelayer,
+    current_height: u64,
+) {
+    // Get all the update actions that are ready to be enacted
+    let actions_to_enact = state.process_queued(current_height);
+
+    for action in actions_to_enact {
+        match action.action() {
+            UpdateAction::Multisig(update) => {
+                state.apply_multisig_update(update.role(), update.config());
+            }
+            UpdateAction::VerifyingKey(update) => match update.kind() {
+                ProofType::Asm => {
+                    // Emit Log
+                }
+                ProofType::OlStf => {
+                    // Send a InterprotoMsg to OL Core subprotocol
+                }
+            },
+            UpdateAction::OperatorSet(_update) => {
+                // Set an InterProtoMsg to the Bridge Subprotocol;
+            }
+            UpdateAction::Sequencer(_update) => {
+                // Send a InterprotoMsg to the Sequencer subprotocol
+            }
+        }
+    }
+}
 
 pub(crate) fn handle_action(
     state: &mut AdministrationSubprotoState,
@@ -38,8 +70,7 @@ pub(crate) fn handle_action(
                 }
                 action => {
                     // For all others add it to queue
-                    let delay = 0; // FIXME: set proper delay
-                    let activation_height = current_height + delay;
+                    let activation_height = current_height + state.confirmation_depth() as u64;
                     let queued_update = QueuedUpdate::new(id, action, activation_height);
                     state.enqueue(queued_update);
                 }
