d19b72aaae3d6052e694d8d714f9a93cb2c01e78
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-09-04 22:34:35 +0545

    refactor: remove Scheduled and Committed Updates from the state

--- crates/asm/subprotocols/administration/src/handler.rs
+++ crates/asm/subprotocols/administration/src/handler.rs
@@ -1,50 +1,17 @@
 use strata_asm_common::MsgRelayer;
 use strata_asm_proto_administration_txs::actions::{MultisigAction, UpdateAction};
 use strata_crypto::multisig::vote::AggregatedVote;
-use strata_primitives::roles::ProofType;
 
 use crate::{
-    error::AdministrationError,
-    state::AdministrationSubprotoState,
-    updates::{queued::QueuedUpdate, scheduled::ScheduledUpdate},
+    error::AdministrationError, state::AdministrationSubprotoState, updates::queued::QueuedUpdate,
 };
-
-pub(crate) fn handle_scheduled_updates(
-    state: &mut AdministrationSubprotoState,
-    _relayer: &mut impl MsgRelayer,
-    current_height: u64,
-) {
-    // Get all the update actions that are ready to be enacted
-    let actions_to_enact = state.process_scheduled(current_height);
-
-    for action in actions_to_enact {
-        match action.action() {
-            UpdateAction::Multisig(update) => {
-                state.apply_multisig_update(update.role(), update.config());
-            }
-            UpdateAction::VerifyingKey(update) => match update.kind() {
-                ProofType::Asm => {
-                    // Emit Log
-                }
-                ProofType::OlStf => {
-                    // Send a InterprotoMsg to OL Core subprotocol
-                }
-            },
-            UpdateAction::OperatorSet(_update) => {
-                // Set an InterProtoMsg to the Bridge Subprotocol;
-            }
-            UpdateAction::Sequencer(_update) => {
-                // Send a InterprotoMsg to the Sequencer subprotocol
-            }
-        }
-    }
-}
 
 pub(crate) fn handle_action(
     state: &mut AdministrationSubprotoState,
     action: MultisigAction,
     vote: AggregatedVote,
     current_height: u64,
+    _relayer: &mut impl MsgRelayer,
 ) -> Result<(), AdministrationError> {
     let role = match &action {
         MultisigAction::Update(update) => update.required_role(),
@@ -66,15 +33,13 @@ pub(crate) fn handle_action(
         MultisigAction::Update(update) => {
             let id = state.next_update_id();
             match update {
-                // If the action is a VerifyingKeyUpdate, queue it to support cancellation
-                UpdateAction::VerifyingKey(_) => {
-                    let queued_update = QueuedUpdate::try_new(id, update, current_height)?;
-                    state.enqueue(queued_update);
+                UpdateAction::Sequencer(_) => {
+                    // TODO: directly apply it without waiting
                 }
-                // For all other actions, directly schedule them for execution
-                _ => {
-                    let scheduled_update = ScheduledUpdate::try_new(id, update, current_height)?;
-                    state.schedule(scheduled_update);
+                action => {
+                    // For all others add it to queue
+                    let queued_update = QueuedUpdate::try_new(id, action, current_height)?;
+                    state.enqueue(queued_update);
                 }
             }
             state.increment_next_update_id();
