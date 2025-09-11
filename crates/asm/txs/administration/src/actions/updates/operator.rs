42677a29c08dde6798b7b6a242d83189f72a4b5d
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-09-11 21:01:36 +0545

    refactor(operator-set-update): rename new_members, old_members to add_members, remove_members

--- crates/asm/txs/administration/src/actions/updates/operator.rs
+++ crates/asm/txs/administration/src/actions/updates/operator.rs
@@ -6,36 +6,36 @@ use strata_primitives::buf::Buf32;
 use crate::error::AdministrationTxParseError;
 
 /// An update to the Bridge Operator Set:
-/// - removes the specified `old_members`
-/// - adds the specified `new_members`
+/// - removes the specified `remove_members`
+/// - adds the specified `add_members`
 #[derive(Clone, Debug, Eq, PartialEq, Arbitrary, BorshDeserialize, BorshSerialize)]
 pub struct OperatorSetUpdate {
-    new_members: Vec<Buf32>,
-    old_members: Vec<Buf32>,
+    add_members: Vec<Buf32>,
+    remove_members: Vec<Buf32>,
 }
 
 impl OperatorSetUpdate {
     /// Creates a new `OperatorSetUpdate`.
-    pub fn new(new_members: Vec<Buf32>, old_members: Vec<Buf32>) -> Self {
+    pub fn new(add_members: Vec<Buf32>, remove_members: Vec<Buf32>) -> Self {
         Self {
-            new_members,
-            old_members,
+            add_members,
+            remove_members,
         }
     }
 
-    /// Borrow the list of new operator public keys to add.
-    pub fn new_members(&self) -> &[Buf32] {
-        &self.new_members
+    /// Borrow the list of operator public keys to add.
+    pub fn add_members(&self) -> &[Buf32] {
+        &self.add_members
     }
 
-    /// Borrow the list of old operator public keys to remove.
-    pub fn old_members(&self) -> &[Buf32] {
-        &self.old_members
+    /// Borrow the list of operator public keys to remove.
+    pub fn remove_members(&self) -> &[Buf32] {
+        &self.remove_members
     }
 
-    /// Consume and return the inner vectors `(new_members, old_members)`.
+    /// Consume and return the inner vectors `(add_members, remove_members)`.
     pub fn into_inner(self) -> (Vec<Buf32>, Vec<Buf32>) {
-        (self.new_members, self.old_members)
+        (self.add_members, self.remove_members)
     }
 
     /// Extracts an `OperatorSetUpdate` from a transaction input.
