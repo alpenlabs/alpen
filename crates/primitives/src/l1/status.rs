0f9697bb36a0a6781217e4306cd32e6615b1e6a6
Jose Storopoli <jose@storopoli.com>
2025-09-25 15:45:17 -0300

    fix(primitives): custom Debug/Display for L1Status

--- crates/primitives/src/l1/status.rs
+++ crates/primitives/src/l1/status.rs
@@ -1,10 +1,13 @@
+use std::fmt;
+
 use arbitrary::Arbitrary;
+use const_hex as hex;
 use serde::{Deserialize, Serialize};
 
 use crate::buf::Buf32;
 
 /// Data that reflects what's happening around L1
-#[derive(Debug, Clone, Serialize, Deserialize, Default, Arbitrary)]
+#[derive(Clone, Serialize, Deserialize, Default, Arbitrary)]
 pub struct L1Status {
     /// If the last time we tried to poll the client (as of `last_update`)
     /// we were successful.
@@ -28,4 +31,49 @@ pub struct L1Status {
 
     /// Number of published reveal transactions.
     pub published_reveal_txs_count: u64,
+}
+
+// Custom debug implementation to print the txid in little endian
+impl fmt::Debug for L1Status {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let last_published_txid_le = self.last_published_txid.map(|txid| {
+            let mut bytes = txid.0;
+            bytes.reverse();
+            hex::encode(bytes)
+        });
+
+        f.debug_struct("L1Status")
+            .field("bitcoin_rpc_connected", &self.bitcoin_rpc_connected)
+            .field("last_rpc_error", &self.last_rpc_error)
+            .field("cur_height", &self.cur_height)
+            .field("cur_tip_blkid", &self.cur_tip_blkid)
+            .field("last_published_txid", &last_published_txid_le)
+            .field("last_update", &self.last_update)
+            .field(
+                "published_reveal_txs_count",
+                &self.published_reveal_txs_count,
+            )
+            .finish()
+    }
+}
+
+// Custom display information to print the txid in little endian
+impl fmt::Display for L1Status {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let last_published_txid_le = self.last_published_txid.map(|txid| {
+            let mut bytes = txid.0;
+            bytes.reverse();
+            hex::encode(bytes)
+        });
+
+        write!(
+            f,
+            "L1Status {{ bitcoin_rpc_connected: {}, cur_height: {}, cur_tip_blkid: {}, last_published_txid: {}, published_reveal_txs_count: {} }}",
+            self.bitcoin_rpc_connected,
+            self.cur_height,
+            self.cur_tip_blkid,
+            last_published_txid_le.as_deref().unwrap_or("None"),
+            self.published_reveal_txs_count
+        )
+    }
 }
