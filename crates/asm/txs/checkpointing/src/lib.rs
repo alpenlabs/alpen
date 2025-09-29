fadc912743401dcf44dc2bae20a860953ecf0a71
Hamid Bateni <hamidmrd79@gmail.com>
2025-09-29 08:08:14 +0400

    asm: fix build issue

--- crates/asm/txs/checkpointing/src/lib.rs
+++ crates/asm/txs/checkpointing/src/lib.rs
@@ -3,9 +3,7 @@
 pub mod constants;
 pub mod errors;
 pub mod parser;
-pub mod tag;
 
 pub use constants::{CHECKPOINTING_V0_SUBPROTOCOL_ID, OL_STF_CHECKPOINT_TX_TYPE};
 pub use errors::{CheckpointTxError, CheckpointTxResult};
 pub use parser::{extract_signed_checkpoint_from_envelope, extract_withdrawal_messages};
-pub use tag::encode_checkpoint_tag;
