9fe84df9a838d2971288123818a15faef553ab52
Trey Del Bonis <trey@alpenlabs.io>
2025-10-03 13:28:15 -0400

    refactor(ledger-types): reworking some ideas, expose MMR in account

--- crates/ledger-types/src/state_accessor.rs
+++ crates/ledger-types/src/state_accessor.rs
@@ -2,31 +2,43 @@ use strata_acct_types::{AccountId, AccountSerial, AcctResult};
 
 use crate::{
     account::{AccountTypeState, IAccountState},
-    toplevel::IToplevelState,
+    global_state::IGlobalState,
 };
 
-/// Opaque interface for manipulating the ledger state.
+/// Opaque interface for manipulating the chainstate, for all of the parts
+/// directly under the toplevel state.
 ///
 /// This exists because we want to make this generic across the various
 /// different contexts we'll be manipulating state.
 pub trait StateAccessor {
-    /// Type representing the toplevel chainstate.
-    type ToplevelState: IToplevelState;
+    /// Type representing the global chainstate.
+    type GlobalState: IGlobalState;
 
-    /// Type representing ledger account state.
+    /// Type representing a ledger account's state.
     type AccountState: IAccountState;
 
-    /// Gets the toplevel state.
-    fn toplevel(&self) -> &Self::ToplevelState;
+    /// Gets a ref to the global state.
+    fn global(&self) -> &Self::GlobalState;
+
+    /// Gets a mut ref to the global state.
+    fn globlal_mut(&mut self) -> &mut Self::GlobalState;
 
     /// Checks if an account exists.
     fn check_account_exists(&self, id: AccountId) -> AcctResult<bool>;
 
-    /// Fetches an account by ID, if it exists.
-    fn fetch_account_state(&self, id: AccountId) -> AcctResult<Option<Self::AccountState>>;
+    /// Gets a ref to an account, if it exists.
+    fn get_account_state(&self, id: AccountId) -> AcctResult<Option<&Self::AccountState>>;
 
-    /// Stores an existing account entry's state, if it exists.
-    fn store_account_state(&mut self, id: AccountId, state: Self::AccountState) -> AcctResult<()>;
+    /// Gets a mut ref to an account, if it exists.
+    fn get_account_state_mut(
+        &mut self,
+        id: AccountId,
+    ) -> AcctResult<Option<&mut Self::AccountState>>;
+
+    /// Overwrites an existing account entry's state, if it exists.
+    ///
+    /// This refuses to create new accounts in order to avoid accidents.
+    fn update_account_state(&mut self, id: AccountId, state: Self::AccountState) -> AcctResult<()>;
 
     /// Creates a new account as some ID with some type state, if that ID
     /// doesn't exist, returning the serial.
