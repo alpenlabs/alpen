03b7c9c860a56fbd0e2fe2234e8dc7be3adff18f
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-10-10 10:57:07 +0545

    refactor: rm risc0-groth16-verifier as dep

--- crates/crypto/src/groth16_verifier.rs
+++ crates/crypto/src/groth16_verifier.rs
@@ -6,7 +6,6 @@ pub fn verify_rollup_groth16_proof_receipt(
     rollup_vk: &RollupVerifyingKey,
 ) -> ZkVmResult<()> {
     match rollup_vk {
-        RollupVerifyingKey::Risc0VerifyingKey(vk) => ZkVmVerifier::verify(vk, proof_receipt),
         RollupVerifyingKey::SP1VerifyingKey(vk) => ZkVmVerifier::verify(vk, proof_receipt),
         // In Native Execution mode, we do not actually generate the proof to verify. Checking
         // public parameters is sufficient.
