0d4b574063749cc5d96eafc7b4fed3230d64cca0
Dilli Raj Paudel <dilli@alpenlabs.io>
2025-10-22 16:18:49 +0545

    feat: acct-types use acct-ssz-types

--- crates/acct-types/src/lib.rs
+++ crates/acct-types/src/lib.rs
@@ -19,8 +19,14 @@ pub use amount::BitcoinAmount;
 pub use constants::SYSTEM_RESERVED_ACCTS;
 pub use errors::{AcctError, AcctResult};
 pub use id::{AccountId, AccountSerial, AccountTypeId, RawAccountTypeId, SubjectId};
-pub use messages::{MsgPayload, ReceivedMessage, SentMessage};
+pub use messages::{MsgPayload, MsgPayloadData, ReceivedMessage, SentMessage};
 pub use mmr::{CompactMmr64, Hash, MerkleProof, Mmr64, RawMerkleProof, StrataHasher};
-pub use state::{AccountState, AccountTypeState, AcctStateSummary, IntrinsicAccountState};
+pub use state::{
+    AccountEncodedState, AccountState, AccountTypeState, AcctStateSummary, IntrinsicAccountState,
+};
 pub use util::compute_codec_sha256;
 pub use varint_vec::{VARINT_MAX, VarVec};
+
+// Re-export SSZ constants
+pub const MAX_MSG_PAYLOAD_DATA_BYTES: usize = 1 << 20; // 1 MiB
+pub const MAX_ACCOUNT_ENCODED_STATE_BYTES: usize = 1 << 16; // 64 KiB
