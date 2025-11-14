f719389e5d4a2e670750454a89ed88790ece3483
Evgeniy Zdanovich <zdanovichevgeny@gmail.com>
2025-11-14 18:38:17 +0100

    refactor(prover-client): Unify input providers with single generic implementation

--- bin/prover-client/src/service/inputs.rs
+++ bin/prover-client/src/service/inputs.rs
@@ -8,12 +8,10 @@ use std::{future::Future, pin::Pin, sync::Arc};
 
 use strata_db_store_sled::prover::ProofDBSled;
 use strata_paas::{InputProvider, PaaSError, PaaSResult};
-use strata_proofimpl_checkpoint::program::{CheckpointProgram, CheckpointProverInput};
-use strata_proofimpl_cl_stf::program::{ClStfInput, ClStfProgram};
-use strata_proofimpl_evm_ee_stf::{primitives::EvmEeProofInput, program::EvmEeProgram};
+use zkaleido::ZkVmProgram;
 
 use crate::errors::ProvingTaskError;
-use crate::operators::{checkpoint::CheckpointOperator, cl_stf::ClStfOperator, evm_ee::EvmEeOperator};
+use crate::operators::{checkpoint::CheckpointOperator, cl_stf::ClStfOperator, evm_ee::EvmEeOperator, ProofInputFetcher};
 
 use super::proof_key_for;
 use super::task::ProofTask;
@@ -49,46 +47,16 @@ impl<O> ProofInputProvider<O> {
     }
 }
 
-// Implementation for CheckpointOperator
-impl InputProvider<ProofTask, CheckpointProgram> for ProofInputProvider<CheckpointOperator> {
+// Generic implementation for any operator implementing ProofInputFetcher
+impl<O, Prog> InputProvider<ProofTask, Prog> for ProofInputProvider<O>
+where
+    O: ProofInputFetcher + Clone + 'static,
+    Prog: ZkVmProgram<Input = O::Input>,
+{
     fn provide_input<'a>(
         &'a self,
         program: &'a ProofTask,
-    ) -> Pin<Box<dyn Future<Output = PaaSResult<CheckpointProverInput>> + Send + 'a>> {
-        Box::pin(async move {
-            let proof_context = program.0;
-            let proof_key = proof_key_for(proof_context);
-            self.operator
-                .fetch_input(&proof_key, &self.db)
-                .await
-                .map_err(to_paas_error)
-        })
-    }
-}
-
-// Implementation for ClStfOperator
-impl InputProvider<ProofTask, ClStfProgram> for ProofInputProvider<ClStfOperator> {
-    fn provide_input<'a>(
-        &'a self,
-        program: &'a ProofTask,
-    ) -> Pin<Box<dyn Future<Output = PaaSResult<ClStfInput>> + Send + 'a>> {
-        Box::pin(async move {
-            let proof_context = program.0;
-            let proof_key = proof_key_for(proof_context);
-            self.operator
-                .fetch_input(&proof_key, &self.db)
-                .await
-                .map_err(to_paas_error)
-        })
-    }
-}
-
-// Implementation for EvmEeOperator
-impl InputProvider<ProofTask, EvmEeProgram> for ProofInputProvider<EvmEeOperator> {
-    fn provide_input<'a>(
-        &'a self,
-        program: &'a ProofTask,
-    ) -> Pin<Box<dyn Future<Output = PaaSResult<EvmEeProofInput>> + Send + 'a>> {
+    ) -> Pin<Box<dyn Future<Output = PaaSResult<Prog::Input>> + Send + 'a>> {
         Box::pin(async move {
             let proof_context = program.0;
             let proof_key = proof_key_for(proof_context);
