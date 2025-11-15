8adcf7f93193e93e7257875bb06ed14f4462e394
Evgeniy Zdanovich <zdanovichevgeny@gmail.com>
2025-11-15 21:26:45 +0100

    chore: Revert unrelated asm changes

--- crates/asm/logs/src/export.rs
+++ crates/asm/logs/src/export.rs
@@ -6,14 +6,7 @@ use strata_msg_fmt::TypeId;
 use crate::constants::NEW_EXPORT_ENTRY_LOG_TYPE;
 
 /// Details for an export state update event.
-///
-/// TODO(PaaS-refactor): Temporarily removed BorshSerialize/BorshDeserialize derives
-/// because moho-types::ExportEntry doesn't implement these traits. This is a known
-/// issue from the main branch rebase. Once moho-types is updated to support Borsh
-/// serialization, restore these derives.
-///
-/// See: https://github.com/alpenlabs/moho (needs Borsh support)
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
 pub struct NewExportEntry {
     /// Export container ID.
     pub container_id: u16,
@@ -28,20 +21,6 @@ impl NewExportEntry {
             container_id,
             entry_data,
         }
-    }
-}
-
-// TODO(PaaS-refactor): Stub implementations for Borsh traits
-// These are temporary workarounds until moho-types supports Borsh serialization
-impl BorshSerialize for NewExportEntry {
-    fn serialize<W: std::io::Write>(&self, _writer: &mut W) -> std::io::Result<()> {
-        unimplemented!("NewExportEntry Borsh serialization requires moho-types Borsh support")
-    }
-}
-
-impl BorshDeserialize for NewExportEntry {
-    fn deserialize_reader<R: std::io::Read>(_reader: &mut R) -> std::io::Result<Self> {
-        unimplemented!("NewExportEntry Borsh deserialization requires moho-types Borsh support")
     }
 }
 
