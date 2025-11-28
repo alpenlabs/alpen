cf6853bb1f4edf4d896cd1e55097496c603057f1
Jose Storopoli <jose@storopoli.com>
2025-11-28 14:41:57 -0300

    feat(reth-node): head gossip rlpx subprotocol

---
+++ crates/reth/node/src/gossip/connection.rs
@@ -0,0 +1,157 @@
+//! Connection handler for the custom RLPx subprotocol.
+use std::{
+    pin::Pin,
+    task::{ready, Context, Poll},
+};
+
+use alloy_primitives::bytes::BytesMut;
+use futures::{Stream, StreamExt};
+use reth_eth_wire::{
+    capability::SharedCapabilities, multiplex::ProtocolConnection, protocol::Protocol,
+};
+use reth_network::protocol::{ConnectionHandler, OnNotSupported};
+use reth_network_api::{Direction, PeerId};
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use tracing::error;
+
+use crate::{
+    gossip::{
+        handler::{AlpenGossipEvent, AlpenGossipState},
+        protocol::alpen_gossip_protocol,
+    },
+    AlpenGossipMessage,
+};
+
+/// Command to send to the connection.
+#[derive(Debug)]
+pub enum AlpenGossipCommand {
+    /// Send a message to the peer.
+    SendMessage(AlpenGossipMessage),
+}
+
+/// Connection handler for the Alpen gossip protocol.
+#[derive(Debug)]
+pub struct AlpenGossipConnectionHandler {
+    /// Alpen gossip state.
+    pub(crate) state: AlpenGossipState,
+}
+
+impl AlpenGossipConnectionHandler {
+    /// Creates a new [`AlpenGossipConnectionHandler`] with the given state.
+    pub fn new(state: AlpenGossipState) -> Self {
+        Self { state }
+    }
+
+    /// Gets the current state of the Alpen gossip protocol.
+    pub fn state(&self) -> &AlpenGossipState {
+        &self.state
+    }
+}
+
+impl ConnectionHandler for AlpenGossipConnectionHandler {
+    type Connection = AlpenGossipConnection;
+
+    fn protocol(&self) -> Protocol {
+        alpen_gossip_protocol()
+    }
+
+    fn on_unsupported_by_peer(
+        self,
+        _supported: &SharedCapabilities,
+        _direction: Direction,
+        _peer_id: PeerId,
+    ) -> OnNotSupported {
+        // make it simple and keep it alive even if other peers do not support
+        OnNotSupported::KeepAlive
+    }
+
+    fn into_connection(
+        self,
+        direction: Direction,
+        peer_id: PeerId,
+        conn: ProtocolConnection,
+    ) -> Self::Connection {
+        let (tx, rx) = mpsc::unbounded_channel();
+        self.state
+            .events
+            .send(AlpenGossipEvent::Established {
+                peer_id,
+                direction,
+                to_connection: tx,
+            })
+            .ok();
+
+        AlpenGossipConnection {
+            conn,
+            commands: UnboundedReceiverStream::new(rx),
+            peer_id,
+            events: self.state.events.clone(),
+        }
+    }
+}
+
+/// Connection for the Alpen gossip protocol.
+#[derive(Debug)]
+pub struct AlpenGossipConnection {
+    /// Protocol connection.
+    conn: ProtocolConnection,
+
+    /// Command stream.
+    commands: UnboundedReceiverStream<AlpenGossipCommand>,
+
+    /// Peer id.
+    peer_id: PeerId,
+
+    /// Event sender.
+    events: mpsc::UnboundedSender<AlpenGossipEvent>,
+}
+
+impl Drop for AlpenGossipConnection {
+    fn drop(&mut self) {
+        self.events
+            .send(AlpenGossipEvent::Closed {
+                peer_id: self.peer_id,
+            })
+            .ok();
+    }
+}
+
+impl Stream for AlpenGossipConnection {
+    type Item = BytesMut;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let this = self.get_mut();
+
+        loop {
+            // Poll for outgoing messages
+            if let Poll::Ready(Some(cmd)) = this.commands.poll_next_unpin(cx) {
+                return match cmd {
+                    AlpenGossipCommand::SendMessage(message) => Poll::Ready(Some(message.encode())),
+                };
+            }
+
+            // Poll for incoming messages
+            let Some(msg) = ready!(this.conn.poll_next_unpin(cx)) else {
+                return Poll::Ready(None);
+            };
+
+            let msg = match AlpenGossipMessage::try_decode(&mut &msg[..]) {
+                Ok(msg) => msg,
+                Err(e) => {
+                    // TODO(@storopoli): maybe disconnect on invalid message or invalid sequencer's
+                    //                   signature.
+                    error!(?e, "failed to decode gossip message");
+                    return Poll::Ready(None);
+                }
+            };
+
+            this.events
+                .send(AlpenGossipEvent::Message {
+                    peer_id: this.peer_id,
+                    message: msg,
+                })
+                .ok();
+        }
+    }
+}
