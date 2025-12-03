27d9e763ce601c5e1cbd7dd0309ea1a19a20a4b4
Sapin Bajracharya <sapin@alpenlabs.io>
2025-12-03 17:47:08 +0545

    feat(alpen-ee/database): add db schema and ops for exec block storage

--- crates/alpen-ee/database/src/sleddb/mod.rs
+++ crates/alpen-ee/database/src/sleddb/mod.rs
@@ -4,4 +4,7 @@ mod schema;
 
 pub(crate) use db::EeNodeDBSled;
 pub(crate) use init::init_db;
-pub(crate) use schema::{AccountStateAtOlBlockSchema, OlBlockAtSlotSchema};
+pub(crate) use schema::{
+    AccountStateAtOlBlockSchema, ExecBlockCanonicalSchema, ExecBlockPayloadSchema, ExecBlockSchema,
+    ExecBlocksAtHeightSchema, OlBlockAtSlotSchema,
+};
