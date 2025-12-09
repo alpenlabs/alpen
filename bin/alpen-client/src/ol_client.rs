a7e48d6be416f14569f2323ac688a0b7017e525a
Sapin Bajracharya <sapin@alpenlabs.io>
2025-12-09 22:51:49 +0545

    feat(alpen-ee): impl SequencerOlClient for DummyOlClient

--- bin/alpen-client/src/ol_client.rs
+++ bin/alpen-client/src/ol_client.rs
@@ -1,7 +1,10 @@
-use alpen_ee_common::{OLChainStatus, OLClient, OLClientError, OLEpochSummary};
+use alpen_ee_common::{
+    OLBlockData, OLChainStatus, OLClient, OLClientError, OLEpochSummary, SequencerOLClient,
+};
 use async_trait::async_trait;
 use strata_identifiers::{Buf32, Epoch, OLBlockCommitment};
 use strata_primitives::EpochCommitment;
+use strata_snark_acct_types::SnarkAccountUpdate;
 
 #[derive(Debug, Default)]
 pub(crate) struct DummyOLClient {}
@@ -23,6 +26,37 @@ impl OLClient for DummyOLClient {
             slot_to_block_commitment(epoch as u64).blkid,
         );
         Ok(OLEpochSummary::new(commitment, commitment, vec![]))
+    }
+}
+
+#[async_trait]
+impl SequencerOLClient for DummyOLClient {
+    async fn chain_status(&self) -> Result<OLChainStatus, OLClientError> {
+        Ok(OLChainStatus {
+            latest: OLBlockCommitment::null(),
+            confirmed: EpochCommitment::null(),
+            finalized: EpochCommitment::null(),
+        })
+    }
+
+    async fn get_inbox_messages(
+        &self,
+        min_slot: u64,
+        max_slot: u64,
+    ) -> Result<Vec<OLBlockData>, OLClientError> {
+        let mut blocks = Vec::with_capacity((max_slot - min_slot + 1) as usize);
+        for slot in min_slot..=max_slot {
+            let commitment = slot_to_block_commitment(slot);
+            blocks.push(OLBlockData {
+                commitment,
+                inbox_messages: vec![],
+            })
+        }
+        Ok(blocks)
+    }
+
+    async fn submit_update(&self, _update: SnarkAccountUpdate) -> Result<(), OLClientError> {
+        Ok(())
     }
 }
 
