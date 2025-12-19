53fb7d49b30dff033f2d49caea6e6f7324f7310d
Dilli Raj Paudel <dilli@alpenlabs.io>
2025-12-19 15:12:23 +0545

    feat(ol/block-assembly): block assembly types

--- crates/ol/block-assembly/src/lib.rs
+++ crates/ol/block-assembly/src/lib.rs
@@ -2,6 +2,8 @@
 
 mod context;
 mod error;
+mod types;
 
 pub use context::{BlockAssemblyContext, BlockAssemblyContextImpl};
 pub use error::BlockAssemblyError;
+pub use types::{BlockCompletionData, BlockGenerationConfig, BlockTemplate, FullBlockTemplate};
