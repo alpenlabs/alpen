6bc1d00398889cafe2f742a8195fbc595a652d08
Sapin Bajracharya <sapin@alpenlabs.io>
2025-12-24 03:54:51 +0545

    feat(rpc-new): add strata_getSnarkAccountState rpc api

--- crates/rpc-new/types/src/lib.rs
+++ crates/rpc-new/types/src/lib.rs
@@ -1,11 +1,15 @@
 //! Types related to OL RPC.
 
+mod account_state;
 mod account_summary;
+mod blocktag;
 mod chain_status;
 mod snark_acct_update;
 mod txn;
 
+pub use account_state::RpcSnarkAccountState;
 pub use account_summary::{RpcAccountBlockSummary, RpcAccountEpochSummary, RpcMessageEntry};
+pub use blocktag::OLBlockOrTag;
 pub use chain_status::RpcOLChainStatus;
 pub use snark_acct_update::RpcSnarkAccountUpdate;
 pub use txn::{
