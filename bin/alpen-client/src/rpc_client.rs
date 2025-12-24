bb17d18160e7692be719d4e8c8178c9bdb9e9248
Jose Storopoli <jose@storopoli.com>
2025-12-24 13:07:38 -0300

    feat(alpen-client): Add retry mechanism for RPC client

--- bin/alpen-client/src/rpc_client.rs
+++ bin/alpen-client/src/rpc_client.rs
@@ -3,7 +3,12 @@ use alpen_ee_common::{
 };
 use async_trait::async_trait;
 use ssz::Encode;
-use strata_common::ws_client::{ManagedWsClient, WsClientConfig};
+use strata_common::{
+    retry::{
+        policies::ExponentialBackoff, retry_with_backoff_async, DEFAULT_ENGINE_CALL_MAX_RETRIES,
+    },
+    ws_client::{ManagedWsClient, WsClientConfig},
+};
 use strata_identifiers::{AccountId, Epoch};
 use strata_rpc_api_new::OLClientRpcClient;
 use strata_rpc_types_new::{
@@ -34,46 +39,60 @@ impl RpcOLClient {
 #[async_trait]
 impl OLClient for RpcOLClient {
     async fn chain_status(&self) -> Result<OLChainStatus, OLClientError> {
-        // TODO: retry on network error
-        let status = self
-            .client
-            .chain_status()
-            .await
-            .map_err(|e| OLClientError::rpc(e.to_string()))?;
+        retry_with_backoff_async(
+            "ol_client_chain_status",
+            DEFAULT_ENGINE_CALL_MAX_RETRIES,
+            &ExponentialBackoff::default(),
+            || async {
+                let status = self
+                    .client
+                    .chain_status()
+                    .await
+                    .map_err(|e| OLClientError::rpc(e.to_string()))?;
 
-        Ok(OLChainStatus {
-            latest: *status.latest(),
-            confirmed: *status.confirmed(),
-            finalized: *status.finalized(),
-        })
+                Ok(OLChainStatus {
+                    latest: *status.latest(),
+                    confirmed: *status.confirmed(),
+                    finalized: *status.finalized(),
+                })
+            },
+        )
+        .await
     }
 
     async fn epoch_summary(&self, epoch: Epoch) -> Result<OLEpochSummary, OLClientError> {
-        // TODO: retry on network error
-        let epoch_summary = self
-            .client
-            .get_acct_epoch_summary(self.account_id, epoch)
-            .await
-            .map_err(|e| OLClientError::rpc(e.to_string()))?;
+        retry_with_backoff_async(
+            "ol_client_epoch_summary",
+            DEFAULT_ENGINE_CALL_MAX_RETRIES,
+            &ExponentialBackoff::default(),
+            || async {
+                let epoch_summary = self
+                    .client
+                    .get_acct_epoch_summary(self.account_id, epoch)
+                    .await
+                    .map_err(|e| OLClientError::rpc(e.to_string()))?;
 
-        let update = UpdateInputData::new(
-            epoch_summary.next_seq_no,
-            epoch_summary
-                .processed_msgs
-                .into_iter()
-                .map(Into::into)
-                .collect(),
-            UpdateStateData::new(
-                epoch_summary.proof_state.into(),
-                epoch_summary.extra_data.into(),
-            ),
-        );
+                let update = UpdateInputData::new(
+                    epoch_summary.next_seq_no,
+                    epoch_summary
+                        .processed_msgs
+                        .into_iter()
+                        .map(Into::into)
+                        .collect(),
+                    UpdateStateData::new(
+                        epoch_summary.proof_state.into(),
+                        epoch_summary.extra_data.into(),
+                    ),
+                );
 
-        Ok(OLEpochSummary::new(
-            epoch_summary.epoch_commitment,
-            epoch_summary.prev_epoch_commitment,
-            vec![update],
-        ))
+                Ok(OLEpochSummary::new(
+                    epoch_summary.epoch_commitment,
+                    epoch_summary.prev_epoch_commitment,
+                    vec![update],
+                ))
+            },
+        )
+        .await
     }
 }
 
@@ -88,27 +107,34 @@ impl SequencerOLClient for RpcOLClient {
         min_slot: u64,
         max_slot: u64,
     ) -> Result<Vec<OLBlockData>, OLClientError> {
-        // TODO: retry on network error
-        let block_summaries = self
-            .client
-            .get_blocks_summaries(self.account_id, min_slot, max_slot)
-            .await
-            .map_err(|e| OLClientError::rpc(e.to_string()))?;
+        retry_with_backoff_async(
+            "ol_client_get_inbox_messages",
+            DEFAULT_ENGINE_CALL_MAX_RETRIES,
+            &ExponentialBackoff::default(),
+            || async {
+                let block_summaries = self
+                    .client
+                    .get_blocks_summaries(self.account_id, min_slot, max_slot)
+                    .await
+                    .map_err(|e| OLClientError::rpc(e.to_string()))?;
 
-        let blocks = block_summaries
-            .into_iter()
-            .map(|block_summary| OLBlockData {
-                commitment: block_summary.block_commitment,
-                next_inbox_msg_idx: block_summary.next_inbox_msg_idx(),
-                inbox_messages: block_summary
-                    .new_inbox_messages
+                let blocks = block_summaries
                     .into_iter()
-                    .map(Into::into)
-                    .collect(),
-            })
-            .collect();
+                    .map(|block_summary| OLBlockData {
+                        commitment: block_summary.block_commitment,
+                        inbox_messages: block_summary
+                            .new_inbox_messages
+                            .into_iter()
+                            .map(Into::into)
+                            .collect(),
+                        next_inbox_msg_idx: block_summary.next_inbox_msg_idx,
+                    })
+                    .collect();
 
-        Ok(blocks)
+                Ok(blocks)
+            },
+        )
+        .await
     }
 
     async fn submit_update(&self, update: SnarkAccountUpdate) -> Result<(), OLClientError> {
@@ -123,12 +149,19 @@ impl SequencerOLClient for RpcOLClient {
             RpcTransactionAttachment::new(None, None),
         );
 
-        // TODO: retry on network error
-        self.client
-            .submit_transaction(tx)
-            .await
-            .map_err(|e| OLClientError::rpc(e.to_string()))?;
+        retry_with_backoff_async(
+            "ol_client_submit_update",
+            DEFAULT_ENGINE_CALL_MAX_RETRIES,
+            &ExponentialBackoff::default(),
+            || async {
+                self.client
+                    .submit_transaction(tx.clone())
+                    .await
+                    .map_err(|e| OLClientError::rpc(e.to_string()))?;
 
-        Ok(())
+                Ok(())
+            },
+        )
+        .await
     }
 }
