1cbb37e36af0052ad648a4cd06d8d924c68dc9cb
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-12-30 09:52:43 +0545

    refactor: rm musig2 type wrappers

--- crates/bridge-types/src/lib.rs
+++ crates/bridge-types/src/lib.rs
@@ -5,14 +5,10 @@
 
 mod bridge;
 mod bridge_ops;
-mod constants;
 mod operator;
 
 // Re-export commonly used types
 #[cfg(not(target_os = "zkvm"))]
 pub use bridge::PublickeyTable;
-pub use bridge::{
-    Musig2PartialSignature, Musig2PubNonce, Musig2SecNonce, OperatorPartialSig, TxSigningData,
-};
 pub use bridge_ops::{DepositIntent, WithdrawalBatch, WithdrawalIntent};
 pub use operator::{OperatorIdx, OperatorKeyProvider, OperatorPubkeys, StubOpKeyProv};
