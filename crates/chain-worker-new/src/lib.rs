736813e7032c05309e3200b97a4d84a73c6b9149
Jose Storopoli <jose@storopoli.com>
2025-12-31 11:24:07 -0300

    feat(chain-worker-new): add ChainWorkerHandle

--- crates/chain-worker-new/src/lib.rs
+++ crates/chain-worker-new/src/lib.rs
@@ -14,12 +14,14 @@
 #![allow(unused, reason = "in development")]
 
 mod errors;
+mod handle;
 mod message;
 mod output;
 mod traits;
 
 use anyhow as _;
 pub use errors::{WorkerError, WorkerResult};
+pub use handle::{ChainWorkerHandle, WorkerShared};
 pub use message::ChainWorkerMessage;
 pub use output::OLBlockExecutionOutput;
 use serde as _;
@@ -30,6 +32,5 @@ use strata_ledger_types as _;
 use strata_params as _;
 use strata_snark_acct_types as _;
 use strata_status as _;
-use tokio as _;
 use tracing as _;
 pub use traits::WorkerContext;
