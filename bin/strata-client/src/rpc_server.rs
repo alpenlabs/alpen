0d52873ce2e71ebe83bd0fa21246951f2938007b
Jose Storopoli <jose@storopoli.com>
2026-01-09 15:23:17 -0300

    revert(strata-client): don't change old code in new PRs

--- bin/strata-client/src/rpc_server.rs
+++ bin/strata-client/src/rpc_server.rs
@@ -612,28 +612,31 @@ impl StrataApiServer for StrataRpcImpl {
             .ok_or(Error::BeforeGenesis)?;
         let cstate = self.status_channel.get_cur_client_state();
 
-        // Finalized check - checkpoint has been buried under enough L1 blocks
-        if let Some(finalized_ckpt) = cstate.get_last_finalized_checkpoint() {
-            if finalized_ckpt
-                .batch_info
-                .l2_slot_at_or_before_end(block_slot)
-            {
-                return Ok(L2BlockStatus::Finalized(
-                    finalized_ckpt.l1_reference.block_height(),
-                ));
-            }
-        }
-
-        // Verified check - checkpoint on L1 but not yet buried
+        // FIXME when this was written, "finalized" just meant included in a
+        // checkpoint, not that the checkpoint was buried, so we're replicating
+        // that behavior here
+        // Finalized check
         if let Some(last_checkpoint) = cstate.get_last_checkpoint() {
             if last_checkpoint
                 .batch_info
                 .l2_slot_at_or_before_end(block_slot)
             {
-                return Ok(L2BlockStatus::Verified(
+                return Ok(L2BlockStatus::Finalized(
                     last_checkpoint.l1_reference.block_height(),
                 ));
             }
+        }
+
+        // Verified check
+        let verified_l1_height = cstate.get_last_checkpoint().and_then(|ckpt| {
+            if ckpt.batch_info.l2_slot_at_or_before_end(block_slot) {
+                Some(ckpt.l1_reference.block_height())
+            } else {
+                None
+            }
+        });
+        if let Some(l1_height) = verified_l1_height {
+            return Ok(L2BlockStatus::Verified(l1_height));
         }
 
         // Confirmed check
