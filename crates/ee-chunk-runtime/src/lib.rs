ce6ae576f30bea7e804622a8d2208ac35e91d173
Trey Del Bonis <trey@alpenlabs.io>
2026-01-23 14:52:23 -0500

    feature(ee-chunk-rt): add more extensive private input types

--- crates/ee-chunk-runtime/src/lib.rs
+++ crates/ee-chunk-runtime/src/lib.rs
@@ -1,2 +1,16 @@
+//! Chunk proof runtime for generic execution environments.
+//!
+//! Proves the state transitions for a chunk of blocks.  Attests to chunk bounds
+//! and execution input/output traces.
+
+// TODO remove these
+extern crate strata_acct_types as _;
+extern crate strata_snark_acct_types as _;
+
 mod chunk;
 mod chunk_processing;
+mod private_inputs;
+
+pub use chunk::*;
+pub use chunk_processing::*;
+pub use private_inputs::*;
