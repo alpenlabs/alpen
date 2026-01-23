c8334a8274f57aec429df1879a5e73ef083048f9
Trey Del Bonis <trey@alpenlabs.io>
2026-01-23 15:09:19 -0500

    feature(ee-chunk-rt): expand chunk processing impl

--- crates/ee-chunk-runtime/src/chunk_processing.rs
+++ crates/ee-chunk-runtime/src/chunk_processing.rs
@@ -1,5 +1,5 @@
 use strata_ee_acct_types::{
-    EnvError, EnvResult, ExecBlock, ExecHeader, ExecPayload, ExecutionEnvironment,
+    EnvError, EnvResult, ExecBlock, ExecHeader, ExecPartialState, ExecPayload, ExecutionEnvironment,
 };
 use strata_ee_chain_types::{ChunkTransition, ExecInputs, ExecOutputs, SequenceTracker};
 
@@ -32,18 +32,24 @@ pub fn process_block<E: ExecutionEnvironment>(
     Ok(())
 }
 
-/// Processes a chunk and updates the state, verifying the final state of the chunk and that it
-/// matches the inputs/outputs described in the transition.
-pub fn process_chunk_transition<E: ExecutionEnvironment>(
+/// Processes a chunk's blocks and updates the state, checking the IO against an
+/// expected IO trace.
+fn process_chunk_blocks<E: ExecutionEnvironment>(
     ee: &E,
     state: &mut E::PartialState,
     chunk: &Chunk<'_, E>,
-    tsn: &ChunkTransition,
+    expected_inputs: &ExecInputs,
+    expected_outputs: &ExecOutputs,
 ) -> EnvResult<()> {
-    // Set up the trackers.
-    let mut deposits_tracker = SequenceTracker::new(tsn.inputs().subject_deposits());
-    let mut out_msg_tracker = SequenceTracker::new(tsn.outputs().output_messages());
-    let mut out_xfr_tracker = SequenceTracker::new(tsn.outputs().output_transfers());
+    // 1. Check that the chunk is nonempty.
+    if chunk.blocks().is_empty() {
+        return Err(EnvError::MalformedChainSegment);
+    }
+
+    // 2. Process each block, tracking the IO traces and chain continuity.
+    let mut deposits_tracker = SequenceTracker::new(expected_inputs.subject_deposits());
+    let mut out_msg_tracker = SequenceTracker::new(expected_outputs.output_messages());
+    let mut out_xfr_tracker = SequenceTracker::new(expected_outputs.output_transfers());
 
     for cb in chunk.blocks() {
         // Verify the block itself.
@@ -61,11 +67,57 @@ pub fn process_chunk_transition<E: ExecutionEnvironment>(
             .map_err(|_| EnvError::InconsistentChunkIo)?;
     }
 
-    // Make sure all the trackers are consumed.
+    // 3. Make sure all the trackers are consumed.
     if !deposits_tracker.is_empty() || !out_msg_tracker.is_empty() || !out_xfr_tracker.is_empty() {
         return Err(EnvError::InconsistentChunkIo);
     }
 
     // TODO
+    Ok(())
+}
+
+/// Verifies a chunk transition using the pre state, parent header, etc.
+pub fn verify_chunk_transition<E: ExecutionEnvironment>(
+    tsn: &ChunkTransition,
+    ee: &E,
+    prev_header: <E::Block as ExecBlock>::Header,
+    state: &mut E::PartialState,
+    chunk: &Chunk<'_, E>,
+) -> EnvResult<()> {
+    // 1. Make sure the parent block ID we have that we're extending from
+    // matches the chunk transition.
+    let computed_prev_blkid = prev_header.compute_block_id();
+    if computed_prev_blkid != tsn.parent_exec_blkid() {
+        // TODO better error type?
+        return Err(EnvError::MismatchedChainSegment);
+    }
+
+    // 2. Make sure the chunk is nonempty and get the last block.
+    let Some(new_tip_header) = chunk.blocks().last().map(|b| b.exec_block().get_header()) else {
+        return Err(EnvError::MalformedChainSegment);
+    };
+
+    let computed_new_tip_blkid = new_tip_header.compute_block_id();
+    if computed_new_tip_blkid != tsn.tip_exec_blkid() {
+        return Err(EnvError::MismatchedChainSegment);
+    }
+
+    // 2. Make sure the state matches the parent block's state root.
+    let computed_pre_sr = state.compute_state_root()?;
+    if computed_pre_sr != prev_header.get_state_root() {
+        return Err(EnvError::MismatchedCurStateData);
+    }
+
+    // 3. Execute the blocks in the chunk.  This dooesn't verify the
+    // intermediate state roots because that's expensive and we only really care
+    // about the final state.
+    process_chunk_blocks(ee, state, chunk, tsn.inputs(), tsn.outputs())?;
+
+    // 4. Compute the final state root and make sure it matches.
+    let computed_post_sr = state.compute_state_root()?;
+    if computed_post_sr != new_tip_header.get_state_root() {
+        return Err(EnvError::MismatchedChainSegment);
+    }
+
     Ok(())
 }
