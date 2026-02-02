d6708e7363a371b86f0af8fd5bba36e3299a6a13
Prajwol Gyawali <prajwol@alpenlabs.io>
2026-02-02 13:17:33 +0545

    feat: add bridge v1 config

---
+++ crates/asm/params/src/bridge.rs
@@ -0,0 +1,19 @@
+use serde::{Deserialize, Serialize};
+use strata_btc_types::BitcoinAmount;
+use strata_crypto::EvenPublicKey;
+
+/// Configuration for the BridgeV1 subprotocol.
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct BridgeV1Config {
+    /// Initial operator MuSig2 public keys for the bridge
+    pub operators: Vec<EvenPublicKey>,
+    /// The amount of bitcoin expected to be locked in the N/N multisig.
+    pub denomination: BitcoinAmount,
+    /// Duration in blocks for assignment execution deadlines
+    pub assignment_duration: u64,
+    /// Amount the operator can take as fees for processing withdrawal.
+    pub operator_fee: BitcoinAmount,
+    /// Number of blocks after Deposit Request Transaction that the depositor can reclaim funds if
+    /// operators fail to process the deposit.
+    pub recovery_delay: u32,
+}
