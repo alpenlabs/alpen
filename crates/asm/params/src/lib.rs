ce246de9ad5c725fe512e972e4a61d9160ad13f6
Prajwol Gyawali <prajwol@alpenlabs.io>
2026-02-02 13:38:00 +0545

    feat: add magic and genesis l1 view

--- crates/asm/params/src/lib.rs
+++ crates/asm/params/src/lib.rs
@@ -1,7 +1,9 @@
 mod admin;
 mod bridge;
 mod checkpoint;
+mod params;
 
 pub use admin::{AdministrationSubprotoParams, Role};
 pub use bridge::BridgeV1Config;
 pub use checkpoint::CheckpointConfig;
+pub use params::AsmParams;
