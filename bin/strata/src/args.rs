60cf8683b5a6b5f65194ca3fa309c86c71ac7f14
Jose Storopoli <jose@storopoli.com>
2026-02-03 14:09:52 -0300

    feat(sequencer): add sequencer CLI args

--- bin/strata/src/args.rs
+++ bin/strata/src/args.rs
@@ -57,6 +57,14 @@ pub(crate) struct Args {
     #[argh(option, description = "rpc port")]
     pub rpc_port: Option<u16>,
 
+    /// Path to sequencer root key (required with `--sequencer`).
+    #[argh(option, short = 'k', description = "path to sequencer root key")]
+    pub sequencer_key: Option<PathBuf>,
+
+    /// Poll interval for duties in ms.
+    #[argh(option, short = 'i', description = "poll interval for duties in ms")]
+    pub duty_poll_interval: Option<u64>,
+
     /// Other generic overrides to the config toml.
     /// Will be used, for example, as `-o btcio.reader.client_poll_dur_ms=1000 -o exec.reth.rpc_url=http://reth`
     #[argh(option, short = 'o', description = "generic config overrides")]
