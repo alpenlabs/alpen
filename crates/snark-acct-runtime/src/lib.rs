de0a13302c6c1aa3c63ab6975fb69ee251548daf
Trey Del Bonis <trey@alpenlabs.io>
2026-02-26 15:47:44 -0500

    feature(snark-acct-rt): expose ledger refs and outputs to program trait

--- crates/snark-acct-runtime/src/lib.rs
+++ crates/snark-acct-runtime/src/lib.rs
@@ -1,13 +1,15 @@
 //! General-purpose snark account runtime library.
 
 mod errors;
+mod ledger;
 mod message;
 mod private_input;
 mod program_processing;
 mod traits;
 
 pub use errors::*;
-pub use message::{InputMessage, MsgMeta};
+pub use ledger::*;
+pub use message::*;
 pub use private_input::*;
 pub use program_processing::*;
 pub use traits::*;
