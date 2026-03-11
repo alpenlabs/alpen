1b94ce952b1e5ff5f160f10b24e13a33c10d8665
voidash <ashish@alpenlabs.io>
2026-03-11 22:07:22 +0545

    fix(docker): store master Xpriv in sequencer.key to avoid double derivation (#1473)
    * fix(docker): store master Xpriv in sequencer.key to avoid double derivation
    
    init-keys.sh was saving the output of `genseqprivkey` (a derived Xpriv
    at m/20000'/10') to sequencer.key. Both `genseqpubkey` and `load_seqkey()`
    then derived m/20000'/10' again, causing double derivation.
    
    STR-2537
    
    * chore: remove unnecessary comment

--- docker/init-keys.sh
+++ docker/init-keys.sh
@@ -60,8 +60,7 @@ $DATATOOL_PATH -b regtest genxpriv -f $OP3_SEED_FILE
 $DATATOOL_PATH -b regtest genxpriv -f $OP4_SEED_FILE
 $DATATOOL_PATH -b regtest genxpriv -f $OP5_SEED_FILE
 
-seqprivkey=$($DATATOOL_PATH -b regtest genseqprivkey -f ${SEQ_SEED_FILE})
-echo -n "$seqprivkey" > $CONFIG_FILE/sequencer.key
+cp "${SEQ_SEED_FILE}" "$CONFIG_FILE/sequencer.key"
 
 op1xpriv=$(cat $OP1_SEED_FILE)
 op2xpriv=$(cat $OP2_SEED_FILE)
