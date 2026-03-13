0ff3e98b45a82ee2ed909f669abd7b0dad17bbe2
Jose Storopoli <jose@storopoli.com>
2026-03-13 14:47:11 -0300

    identifiers: derive SSZ for certain types

--- crates/identifiers/src/cred_rule.rs
+++ crates/identifiers/src/cred_rule.rs
@@ -2,12 +2,25 @@
 
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
+use ssz_derive::{Decode as DeriveDecode, Encode as DeriveEncode};
 
 use crate::Buf32;
 
 /// Rule we use to decide how to identify if an L2 block is correctly signed.
-#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize, BorshSerialize, BorshDeserialize)]
+#[derive(
+    Clone,
+    Debug,
+    PartialEq,
+    Eq,
+    Deserialize,
+    Serialize,
+    BorshSerialize,
+    BorshDeserialize,
+    DeriveEncode,
+    DeriveDecode,
+)]
 #[serde(rename_all = "snake_case")]
+#[ssz(enum_behaviour = "union")]
 pub enum CredRule {
     /// Any block gets accepted, unconditionally.
     Unchecked,
