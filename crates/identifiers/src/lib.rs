f5723c1051a633c06229610d2ebecc8b5e566012
Prajwol Gyawali <prajwol@alpenlabs.io>
2026-03-13 12:23:25 +0545

    refactor: move DepositDescriptor to strata-bridge-types

--- crates/identifiers/src/lib.rs
+++ crates/identifiers/src/lib.rs
@@ -5,7 +5,6 @@ mod macros;
 
 mod acct;
 mod buf;
-mod deposit;
 mod epoch;
 mod exec;
 pub mod hash;
@@ -21,10 +20,9 @@ pub mod test_utils;
 
 pub use acct::{
     AccountId, AccountSerial, AccountTypeId, RawAccountTypeId, SUBJ_ID_LEN, SYSTEM_RESERVED_ACCTS,
-    SubjectId, SubjectIdBytes,
+    SubjectId, SubjectIdBytes, SubjectIdBytesError,
 };
 pub use buf::{Buf20, Buf32, Buf64, RBuf32};
-pub use deposit::{DepositDescriptor, DepositDescriptorError};
 pub use exec::{
     EVMExtraPayload, EvmEeBlockCommitment, ExecBlockCommitment, create_evm_extra_payload,
 };
