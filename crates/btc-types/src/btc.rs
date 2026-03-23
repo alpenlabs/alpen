27f59d76d053b687085c1b766c6138d373f6cf5a
Prajwol Gyawali <prajwol@alpenlabs.io>
2026-03-23 15:40:00 +0545

    fix: update ssz wrapper for BitcoinXOnlyPublicKey

--- crates/btc-types/src/btc.rs
+++ crates/btc-types/src/btc.rs
@@ -28,7 +28,6 @@ use crate::ParseError;
 const HASH_SIZE: usize = 32;
 const BITCOIN_OUTPOINT_LEN: usize = 36;
 const BITCOIN_TXID_LEN: usize = 32;
-const BITCOIN_XONLY_PUBLIC_KEY_LEN: usize = 32;
 
 /// L1 output reference.
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize)]
@@ -618,7 +617,7 @@ impl TryFrom<BitcoinXOnlyPublicKey> for Descriptor {
     }
 }
 
-impl_ssz_transparent_wrapper!(BitcoinXOnlyPublicKey, Buf32, BITCOIN_XONLY_PUBLIC_KEY_LEN);
+impl_ssz_transparent_wrapper!(BitcoinXOnlyPublicKey, Buf32);
 
 /// Represents a raw, byte-encoded Bitcoin transaction with custom [`Arbitrary`] support.
 /// Provides conversions (via [`TryFrom`]) to and from [`Transaction`].
