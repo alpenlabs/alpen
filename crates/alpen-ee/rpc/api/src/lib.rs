4ccaa802cd1a4cb29fec1fb3f1d7164d4a30417b
Dilli Raj Paudel <dilli@alpenlabs.io>
2026-03-26 17:10:10 -0500

    feat(ee-rpc): add AlpenClientRpc trait in crates/alpen-ee/rpc/api

---
+++ crates/alpen-ee/rpc/api/src/lib.rs
@@ -0,0 +1,14 @@
+//! Alpen EE RPC API definitions.
+
+use alloy_primitives::B256;
+pub use alpen_ee_rpc_types::{BlockStatus, BlockStatusResponse};
+use jsonrpsee::{core::RpcResult, proc_macros::rpc};
+
+/// RPC methods exposed by Alpen EE nodes.
+#[cfg_attr(not(feature = "client"), rpc(server, namespace = "strataee"))]
+#[cfg_attr(feature = "client", rpc(server, client, namespace = "strataee"))]
+pub trait AlpenEeRpc {
+    /// Returns the L1 finalization status for an EE block.
+    #[method(name = "getBlockStatus")]
+    async fn get_block_status(&self, block_hash: B256) -> RpcResult<BlockStatusResponse>;
+}
