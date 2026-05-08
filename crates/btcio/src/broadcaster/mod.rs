ec34f15fb884e6ce90e3b9580ddd35489821af9e
Hamid Bateni <bat3ni@gmail.com>
2026-05-08 08:23:45 +0400

    chore(ee-da): lint

--- crates/btcio/src/broadcaster/mod.rs
+++ crates/btcio/src/broadcaster/mod.rs
@@ -10,6 +10,5 @@ mod state;
 pub use builder::BroadcasterBuilder;
 pub use error::BroadcasterError;
 pub use handle::L1BroadcastHandle;
-pub use service::BroadcasterStatus;
-
 pub(crate) use io::is_benign_minus25_message;
+pub use service::BroadcasterStatus;
