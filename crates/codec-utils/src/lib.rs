835506411f965f39ca477c42da7cf918b2c33c9f
Trey Del Bonis <trey@alpenlabs.io>
2026-06-10 15:22:30 -0400

    feature(codec-utils): add `SerdeSsz` serialization wrapper type

--- crates/codec-utils/src/lib.rs
+++ crates/codec-utils/src/lib.rs
@@ -1,7 +1,9 @@
 //! Utils around the strata-codec system that don't belong in the upstream crates.
 
 mod borsh_shim;
+mod serde_ssz;
 mod ssz_shim;
 
 pub use borsh_shim::*;
+pub use serde_ssz::*;
 pub use ssz_shim::*;
