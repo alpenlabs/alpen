d099d20de51cdde3167d13f4a3d880184e26d3a9
barakshani <35497694+barakshani@users.noreply.github.com>
2026-07-13 18:31:48 +0200

    feat!(alpen-cli): remove faucet command (#2109)
    feat(alpen-cli)!: remove faucet command
    
    Co-authored-by: barakshani <barak@alpenlabs.io>

--- bin/alpen-cli/src/main.rs
+++ bin/alpen-cli/src/main.rs
@@ -14,8 +14,8 @@ use std::process::exit;
 
 use cmd::{
     backup::backup, balance::balance, config::config, deposit::deposit, drain::drain,
-    faucet::faucet, receive::receive, recover::recover, scan::scan, send::send, withdraw::withdraw,
-    Commands, TopLevel,
+    receive::receive, recover::recover, scan::scan, send::send, withdraw::withdraw, Commands,
+    TopLevel,
 };
 #[cfg(not(feature = "test-mode"))]
 use cmd::{change_pwd::change_pwd, reset::reset};
@@ -74,7 +74,6 @@ async fn main() {
         Commands::Backup(args) => backup(args, seed).await,
         Commands::Deposit(args) => deposit(args, seed, settings).await,
         Commands::Withdraw(args) => withdraw(args, seed, settings).await,
-        Commands::Faucet(args) => faucet(args, seed, settings).await,
         Commands::Send(args) => send(args, seed, settings).await,
         Commands::Receive(args) => receive(args, seed, settings).await,
         #[cfg(not(feature = "test-mode"))]
