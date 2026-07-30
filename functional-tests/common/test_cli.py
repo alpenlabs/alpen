17f4da63cc49bc9e1a7ff7dc7a230fa7ff5da7b7
Prajwol Gyawali <prajwol@alpenlabs.io>
2026-07-30 09:33:26 +0545

    test(functional): remove leftover EE predicate transition test scaffolding
    el_ol_ee_predicate_transition and create_ee_predicate_update were only
    used by the EE predicate rotation functional tests removed in 4f353806b.

--- functional-tests/common/test_cli.py
+++ functional-tests/common/test_cli.py
@@ -79,39 +79,6 @@ def build_snark_withdrawal(
     return json.loads(result)
 
 
-def create_ee_predicate_update(
-    seq_no: int,
-    predicate: str,
-    admin_xpriv: str,
-    btc_url: str,
-    btc_user: str,
-    btc_password: str,
-    fee_rate: int = 2,
-    commit_output_sats: int = 20_000,
-) -> dict:
-    """Broadcast an admin EE predicate update commit/reveal transaction pair.
-
-    Returns:
-        JSON dict with commit_txid and reveal_txid.
-    """
-    # fmt: off
-    args = [
-        "create-ee-predicate-update",
-        "--seq-no", str(seq_no),
-        "--predicate", predicate,
-        "--admin-xpriv", admin_xpriv,
-        "--btc-url", btc_url,
-        "--btc-user", btc_user,
-        "--btc-password", btc_password,
-        "--fee-rate", str(fee_rate),
-        "--commit-output-sats", str(commit_output_sats),
-    ]
-    # fmt: on
-
-    result = _run_command(args)
-    return json.loads(result)
-
-
 def create_checkpoint_predicate_update(
     seq_no: int,
     predicate: str,
