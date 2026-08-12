0cedb80eddb1fec7d95b8fa7b647ab36ec504525
Purushotam <purushotam@alpenlabs.io>
2026-08-12 16:29:47 +0545

    feat(checkpoint): bind runtime params into guest

--- provers/sp1/guest-checkpoint/src/main.rs
+++ provers/sp1/guest-checkpoint/src/main.rs
@@ -1,9 +1,16 @@
 #![no_main]
 zkaleido_sp1_guest_env::entrypoint!(main);
 
+use ssz::Decode;
+use strata_ol_params::OLRuntimeParams;
 use strata_proofimpl_checkpoint::process_ol_stf;
 use zkaleido_sp1_guest_env::Sp1ZkVmEnv;
 
+mod runtime_params;
+
 fn main() {
-    process_ol_stf(&Sp1ZkVmEnv)
+    let runtime_params =
+        OLRuntimeParams::from_ssz_bytes(runtime_params::CHECKPOINT_RUNTIME_PARAMS_SSZ)
+            .expect("embedded checkpoint runtime params must decode");
+    process_ol_stf(&Sp1ZkVmEnv, runtime_params)
 }
