2ca4858abe341fbce771499498969b1fb16712d2
Trey Del Bonis <trey@alpenlabs.io>
2026-08-26 15:11:02 -0400

    feature(ol/gchain): add OL chainspec impl

--- crates/ol/gchain-impl/src/lib.rs
+++ crates/ol/gchain-impl/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! Implementation of the gchain traits for the orchestration layer.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod chain_spec;
+mod graph_types;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use chain_spec::OLChainSpec;
+pub use graph_types::*;
